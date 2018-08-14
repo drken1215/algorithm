8d6b45718ee2b80a8aaea1cbddbdabd5c79b5b6a
Kensuke Otsuki <dr.ken.1215@gmail.com>
2018-08-15 02:08:41 +0900

    add some geometry

---
+++ Geometry/Projection.cpp
@@ -0,0 +1,77 @@
+//
+// Projction and Reflection
+//
+// verified:
+//   AOJ Course CGL_1_A: Projection
+//   AOJ Course CGL_1_B: Reflection
+//
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+/* Point */
+using DD = double;
+const DD INF = 1LL<<60;      // to be set appropriately
+const DD EPS = 1e-10;        // to be set appropriately
+const DD PI = acos(-1.0);
+DD torad(int deg) {return (DD)(deg) * PI / 180;}
+DD todeg(DD ang) {return ang * 180 / PI;}
+
+struct Point {
+    DD x, y;
+    Point(DD x = 0.0, DD y = 0.0) : x(x), y(y) {}
+    friend ostream& operator << (ostream &s, const Point &p) {return s << '(' << p.x << ", " << p.y << ')';}
+};
+
+inline Point operator + (const Point &p, const Point &q) {return Point(p.x + q.x, p.y + q.y);}
+inline Point operator - (const Point &p, const Point &q) {return Point(p.x - q.x, p.y - q.y);}
+inline Point operator * (const Point &p, DD a) {return Point(p.x * a, p.y * a);}
+inline Point operator * (DD a, const Point &p) {return Point(a * p.x, a * p.y);}
+inline Point operator * (const Point &p, const Point &q) {return Point(p.x * q.x - p.y * q.y, p.x * q.y + p.y * q.x);}
+inline Point operator / (const Point &p, DD a) {return Point(p.x / a, p.y / a);}
+inline Point conj(const Point &p) {return Point(p.x, -p.y);}
+inline Point rot(const Point &p, DD ang) {return Point(cos(ang) * p.x - sin(ang) * p.y, sin(ang) * p.x + cos(ang) * p.y);}
+inline Point rot90(const Point &p) {return Point(-p.y, p.x);}
+inline DD cross(const Point &p, const Point &q) {return p.x * q.y - p.y * q.x;}
+inline DD dot(const Point &p, const Point &q) {return p.x * q.x + p.y * q.y;}
+inline DD norm(const Point &p) {return dot(p, p);}
+inline DD abs(const Point &p) {return sqrt(dot(p, p));}
+inline DD amp(const Point &p) {DD res = atan2(p.y, p.x); if (res < 0) res += PI*2; return res;}
+inline bool eq(const Point &p, const Point &q) {return abs(p - q) < EPS;}
+inline bool operator < (const Point &p, const Point &q) {return (abs(p.x - q.x) > EPS ? p.x < q.x : p.y < q.y);}
+inline bool operator > (const Point &p, const Point &q) {return (abs(p.x - q.x) > EPS ? p.x > q.x : p.y > q.y);}
+inline Point operator / (const Point &p, const Point &q) {return p * conj(q) / norm(q);}
+
+/* Line */
+struct Line : vector<Point> {
+    Line(Point a = Point(0.0, 0.0), Point b = Point(0.0, 0.0)) {
+        this->push_back(a);
+        this->push_back(b);
+    }
+    friend ostream& operator << (ostream &s, const Line &l) {return s << '{' << l[0] << ", " << l[1] << '}';}
+};
+
+/* Projection */
+Point proj(const Point &p, const Line &l) {
+    DD t = dot(p - l[0], l[1] - l[0]) / norm(l[1] - l[0]);
+    return l[0] + (l[1] - l[0]) * t;
+}
+Point refl(const Point &p, const Line &l) {
+    return p + (proj(p, l) - p) * 2;
+}
+
+
+int main() {
+    DD xp1, yp1, xp2, yp2; cin >> xp1 >> yp1 >> xp2 >> yp2;
+    Line l(Point(xp1, yp1), Point(xp2, yp2));
+    int Q; cin >> Q;
+    for (int qu = 0; qu < Q; ++qu) {
+        DD x, y; cin >> x >> y;
+        Point p(x, y);
+        Point res = proj(p, l);
+        cout << fixed << setprecision(10) << res.x << " " << res.y << endl;
+    }
+}
