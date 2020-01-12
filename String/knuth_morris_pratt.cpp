d41481f5011e8b129128050627faddebe2add91a
Kensuke Otsuki <dr.ken.1215@gmail.com>
2020-01-12 12:07:08 +0900

    modify KMP

--- String/knuth_morris_pratt.cpp
+++ String/knuth_morris_pratt.cpp
@@ -7,6 +7,9 @@
 //     ・文字列 S において S[i:] の prefix が pat と一致するような i をすべて求める
 //
 // verified:
+//   ABC 150 F - Xor Shift
+//     https://atcoder.jp/contests/abc150/tasks/abc150_f
+//
 //   ARC 060 F - 最良表現
 //     https://atcoder.jp/contests/arc060/tasks/arc060_d
 //
@@ -14,18 +17,16 @@
 
 #include <iostream>
 #include <vector>
-#include <string>
-#include <algorithm>
 using namespace std;
 
-
-struct KMP {
-    string pat;
+// T = string or vector<long long>
+template<class T> struct KMP {
+    T pat;
     vector<int> fail;
 
     // construct
-    KMP(const string &p) { init(p); }
-    void init(const string &p) {
+    KMP(const T &p) { init(p); }
+    void init(const T &p) {
         pat = p;
         int m = (int)pat.size();
         fail.assign(m+1, -1);
@@ -39,58 +40,40 @@ struct KMP {
     int period(int i) { return i - fail[i]; }
     
     // the index i such that S[i:] has the exact prefix p
-    vector<int> match(const string &S) {
+    vector<int> match(const T &S) {
         int n = (int)S.size(), m = (int)pat.size();
         vector<int> res;
         for (int i = 0, k = 0; i < n; ++i) {
             while (k >= 0 && S[i] != pat[k]) k = fail[k];
             ++k;
-            if (k == m) res.push_back(i-m+1);
+            if (k >= m) res.push_back(i - m + 1), k = fail[k];
         }
         return res;
     }
 };
 
 
-vector<long long> divisor(long long n) {
-    vector<long long> res;
-    for (long long i = 1LL; i*i <= n; ++i) {
-        if (n%i == 0LL) {
-            res.push_back(i);
-            long long temp = n/i;
-            if (i != temp) res.push_back(temp);
-        }
+void solve(int N, const vector<long long> &a, const vector<long long> &b) {
+    vector<long long> da(N*2), db(N);
+    for (int i = 0; i < N*2; ++i) {
+        int j = i % N;
+        int k = (i + 1) % N;
+        da[i] = a[j] ^ a[k];
+        if (i < N) db[i] = b[j] ^ b[k];
     }
-    sort(res.begin(), res.end());
-    return res;
+    KMP<vector<long long> > kmp(db);
+    auto v = kmp.match(da);
+    for (auto k : v) {
+        if (k < N) cout << k << " " << (a[k] ^ b[0]) << endl;
+    }
 }
 
 int main() {
-    string str; cin >> str;
-    int n = (int)str.size();
-    vector<long long> divs = divisor(n);
-    long long syuuki = n;
-    for (auto d : divs) {
-        bool ok = true;
-        for (int j = 0; j + d < n; ++j) {
-            if (str[j] != str[j+d]) ok = false;
-        }
-        if (ok) syuuki = min(syuuki, d);
-    }
-    if (syuuki == n) cout << 1 << endl << 1 << endl;
-    else if (syuuki == 1) cout << n << endl << 1 << endl;
-    else {
-        vector<int> cannot_cut(n*2, 0);
-        string str2 = str;
-        reverse(str2.begin(), str2.end());
-        KMP kmp1(str);
-        KMP kmp2(str2);
-        for (int d = 1; d < n; ++d) {
-            if (kmp1.period(d) < d && d % kmp1.period(d) == 0) cannot_cut[d] = true;
-            if (kmp2.period(d) < d && d % kmp2.period(d) == 0) cannot_cut[n-d] = true;
-        }
-        int con = 0;
-        for (int i = 1; i < n; ++i) if (!cannot_cut[i]) ++con;
-        cout << 2 << endl << con << endl;
-    }
+    int N;
+    cin >> N;
+    vector<long long> a(N), b(N);
+    for (int i = 0; i < N; ++i) cin >> a[i];
+    for (int i = 0; i < N; ++i) cin >> b[i];
+    solve(N, a, b);
 }
+
