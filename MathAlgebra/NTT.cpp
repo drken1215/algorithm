da8134e31d15f7ed3f22f895d05d637f6ba83f27
Kensuke Otsuki <dr.ken.1215@gmail.com>
2020-09-19 01:03:45 +0900

    modify NTT which can be applied mod 1000000007

--- MathAlgebra/NTT.cpp
+++ MathAlgebra/NTT.cpp
@@ -1,120 +1,299 @@
 //
 // NTT (Number Theoretic Transform)
 //
-// cf.
-//
-//
 // verified:
-//   Educational Codeforces Round 057 G - Lucky Tickets
-//     https://codeforces.com/contest/1096/problem/G
+//   Yosupo Judge Convolution (mod 1,000,000,007)
+//     https://judge.yosupo.jp/problem/convolution_mod_1000000007
 //
+
 
 #include <iostream>
 #include <vector>
 using namespace std;
 
 
-
-long long modpow(long long a, long long n, long long mod) {
-    long long res = 1;
-    while (n > 0) {
-        if (n & 1) res = res * a % mod;
-        a = a * a % mod;
-        n >>= 1;
+// modint
+template<int MOD> struct Fp {
+    long long val;
+    constexpr Fp(long long v = 0) noexcept : val(v % MOD) {
+        if (val < 0) val += MOD;
     }
-    return res;
-}
-
-long long modinv(long long a, long long mod) {
-    long long b = mod, u = 1, v = 0;
-    while (b) {
-        long long t = a/b;
-        a -= t*b; swap(a, b);
-        u -= t*v; swap(u, v);
+    constexpr int getmod() const { return MOD; }
+    constexpr Fp operator - () const noexcept {
+        return val ? MOD - val : 0;
     }
-    u %= mod;
-    if (u < 0) u += mod;
-    return u;
-}
+    constexpr Fp operator + (const Fp& r) const noexcept { return Fp(*this) += r; }
+    constexpr Fp operator - (const Fp& r) const noexcept { return Fp(*this) -= r; }
+    constexpr Fp operator * (const Fp& r) const noexcept { return Fp(*this) *= r; }
+    constexpr Fp operator / (const Fp& r) const noexcept { return Fp(*this) /= r; }
+    constexpr Fp& operator += (const Fp& r) noexcept {
+        val += r.val;
+        if (val >= MOD) val -= MOD;
+        return *this;
+    }
+    constexpr Fp& operator -= (const Fp& r) noexcept {
+        val -= r.val;
+        if (val < 0) val += MOD;
+        return *this;
+    }
+    constexpr Fp& operator *= (const Fp& r) noexcept {
+        val = val * r.val % MOD;
+        return *this;
+    }
+    constexpr Fp& operator /= (const Fp& r) noexcept {
+        long long a = r.val, b = MOD, u = 1, v = 0;
+        while (b) {
+            long long t = a / b;
+            a -= t * b; swap(a, b);
+            u -= t * v; swap(u, v);
+        }
+        val = val * u % MOD;
+        if (val < 0) val += MOD;
+        return *this;
+    }
+    constexpr bool operator == (const Fp& r) const noexcept {
+        return this->val == r.val;
+    }
+    constexpr bool operator != (const Fp& r) const noexcept {
+        return this->val != r.val;
+    }
+    friend constexpr istream& operator >> (istream &is, Fp<MOD>& x) noexcept {
+        is >> x.val;
+        x.val %= MOD;
+        if (x.val < 0) x.val += MOD;
+        return is;
+    }
+    friend constexpr ostream& operator << (ostream &os, const Fp<MOD>& x) noexcept {
+        return os << x.val;
+    }
+    friend constexpr Fp<MOD> modpow(const Fp<MOD> &a, long long n) noexcept {
+        if (n == 0) return 1;
+        auto t = modpow(a, n / 2);
+        t = t * t;
+        if (n & 1) t = t * a;
+        return t;
+    }
+};
 
 namespace NTT {
-    const int MOD = 998244353;  // to be set appropriately
-    const long long PR = 3;     // to be set appropriately
-    
-    void trans(vector<long long> &v, bool inv = false) {
-        int n = (int)v.size();
-        for (int i = 0, j = 1; j < n-1; j++) {
-            for (int k = n>>1; k > (i ^= k); k >>= 1);
+    constexpr long long modpow(long long a, long long n, int mod) {
+        long long res = 1;
+        while (n > 0) {
+            if (n & 1) res = res * a % mod;
+            a = a * a % mod;
+            n >>= 1;
+        }
+        return res;
+    }
+
+    constexpr long long modinv(long long a, int mod) {
+        long long b = mod, u = 1, v = 0;
+        while (b) {
+            long long t = a / b;
+            a -= t * b, swap(a, b);
+            u -= t * v, swap(u, v);
+        }
+        u %= mod;
+        if (u < 0) u += mod;
+        return u;
+    }
+
+    constexpr int calc_primitive_root(int mod) {
+        if (mod == 2) return 1;
+        if (mod == 167772161) return 3;
+        if (mod == 469762049) return 3;
+        if (mod == 754974721) return 11;
+        if (mod == 998244353) return 3;
+        int divs[20] = {};
+        divs[0] = 2;
+        int cnt = 1;
+        long long x = (mod - 1) / 2;
+        while (x % 2 == 0) x /= 2;
+        for (long long i = 3; i * i <= x; i += 2) {
+            if (x % i == 0) {
+                divs[cnt++] = i;
+                while (x % i == 0) x /= i;
+            }
+        }
+        if (x > 1) divs[cnt++] = x;
+        for (int g = 2;; g++) {
+            bool ok = true;
+            for (int i = 0; i < cnt; i++) {
+                if (modpow(g, (mod - 1) / divs[i], mod) == 1) {
+                    ok = false;
+                    break;
+                }
+            }
+            if (ok) return g;
+        }
+    }
+
+    constexpr int get_fft_size(int N, int M) {
+        int size_a = 1, size_b = 1;
+        while (size_a < N) size_a <<= 1;
+        while (size_b < M) size_b <<= 1;
+        return max(size_a, size_b) << 1;
+    }
+
+    // number-theoretic transform
+    template<class mint> void trans(vector<mint> &v, bool inv = false) {
+        if (v.empty()) return;
+        int N = (int)v.size();
+        int MOD = v[0].getmod();
+        int PR = calc_primitive_root(MOD);
+        static bool first = true;
+        static vector<long long> vbw(30), vibw(30);
+        if (first) {
+            first = false;
+            for (int k = 0; k < 30; ++k) {
+                vbw[k] = modpow(PR, (MOD - 1) >> (k + 1), MOD);
+                vibw[k] = modinv(vbw[k], MOD);
+            }
+        }
+        for (int i = 0, j = 1; j < N - 1; j++) {
+            for (int k = N >> 1; k > (i ^= k); k >>= 1);
             if (i > j) swap(v[i], v[j]);
         }
-        for (int t = 2; t <= n; t <<= 1) {
-            long long bw = modpow(PR, (MOD-1)/t, MOD);
-            if (inv) bw = modinv(bw, MOD);
-            for (int i = 0; i < n; i += t) {
-                long long w = 1;
+        for (int k = 0, t = 2; t <= N; ++k, t <<= 1) {
+            long long bw = vbw[k];
+            if (inv) bw = vibw[k];
+            for (int i = 0; i < N; i += t) {
+                mint w = 1;
                 for (int j = 0; j < t/2; ++j) {
                     int j1 = i + j, j2 = i + j + t/2;
-                    long long c1 = v[j1], c2 = v[j2] * w % MOD;
+                    mint c1 = v[j1], c2 = v[j2] * w;
                     v[j1] = c1 + c2;
-                    v[j2] = c1 - c2 + MOD;
-                    while (v[j1] >= MOD) v[j1] -= MOD;
-                    while (v[j2] >= MOD) v[j2] -= MOD;
-                    w = w * bw % MOD;
+                    v[j2] = c1 - c2;
+                    w *= bw;
                 }
             }
         }
         if (inv) {
-            long long inv_n = modinv(n, MOD);
-            for (int i = 0; i < n; ++i) v[i] = v[i] * inv_n % MOD;
+            long long invN = modinv(N, MOD);
+            for (int i = 0; i < N; ++i) v[i] = v[i] * invN;
         }
     }
+
+    // small case (T = mint, long long)
+    template<class T> vector<T> naive_mul 
+    (const vector<T> &A, const vector<T> &B) {
+        if (A.empty() || B.empty()) return {};
+        int N = (int)A.size(), M = (int)B.size();
+        vector<T> res(N + M - 1);
+        for (int i = 0; i < N; ++i)
+            for (int j = 0; j < M; ++j)
+                res[i + j] += A[i] * B[j];
+        return res;
+    }
+
+    // case of good mod (typically, mod 998244353)
+    template<class mint> vector<mint> operator * 
+    (const vector<mint> &A, const vector<mint> &B) {
+        if (A.empty() || B.empty()) return {};
+        int N = (int)A.size(), M = (int)B.size();
+        if (min(N, M) < 30) return naive_mul(A, B);
+        int size_fft = get_fft_size(N, M);
+        vector<mint> a(size_fft), b(size_fft), c(size_fft);
+        for (int i = 0; i < N; ++i) a[i] = A[i];
+        for (int i = 0; i < M; ++i) b[i] = B[i];
+        vector<mint> res(size_fft);
+        trans(a), trans(b);
+        for (int i = 0; i < size_fft; ++i) res[i] = a[i] * b[i];
+        trans(res, true);
+        res.resize(N + M - 1);
+        return res;
+    }
+
+    // cgeneral case (mint, long long)
+    static constexpr int MOD0 = 754974721;
+    static constexpr int MOD1 = 167772161;
+    static constexpr int MOD2 = 469762049;
+    using mint0 = Fp<MOD0>;
+    using mint1 = Fp<MOD1>;
+    using mint2 = Fp<MOD2>;
+    static const mint1 imod0 = modinv(MOD0, MOD1);
+    static const mint2 imod1 = modinv(MOD1, MOD2);
+    static const mint2 imod01 = imod1 / MOD0;
     
-    // C is A*B
-    vector<long long> mult(vector<long long> A, vector<long long> B) {
-        int size_a = 1; while (size_a < A.size()) size_a <<= 1;
-        int size_b = 1; while (size_b < B.size()) size_b <<= 1;
-        int size_fft = max(size_a, size_b) << 1;
-        
-        vector<long long> cA(size_fft, 0), cB(size_fft, 0), cC(size_fft, 0);
-        for (int i = 0; i < A.size(); ++i) cA[i] = A[i];
-        for (int i = 0; i < B.size(); ++i) cB[i] = B[i];
-        
-        trans(cA); trans(cB);
-        for (int i = 0; i < size_fft; ++i) cC[i] = cA[i] * cB[i] % MOD;
-        trans(cC, true);
-        
-        vector<long long> res((int)A.size() + (int)B.size() - 1);
-        for (int i = 0; i < res.size(); ++i) res[i] = cC[i];
+    // mint (typically, mod 1000000007)
+    template<class mint> vector<mint> mul
+    (const vector<mint> &A, const vector<mint> &B) {
+        if (A.empty() || B.empty()) return {};
+        int N = (int)A.size(), M = (int)B.size();
+        if (min(N, M) < 30) return naive_mul(A, B);
+        int size_fft = get_fft_size(N, M);
+        vector<mint0> a0(size_fft, 0), b0(size_fft, 0), c0(size_fft, 0);
+        vector<mint1> a1(size_fft, 0), b1(size_fft, 0), c1(size_fft, 0);
+        vector<mint2> a2(size_fft, 0), b2(size_fft, 0), c2(size_fft, 0);
+        for (int i = 0; i < N; ++i)
+            a0[i] = A[i].val, a1[i] = A[i].val, a2[i] = A[i].val;
+        for (int i = 0; i < M; ++i)
+            b0[i] = B[i].val, b1[i] = B[i].val, b2[i] = B[i].val;
+        trans(a0), trans(a1), trans(a2), trans(b0), trans(b1), trans(b2);
+        for (int i = 0; i < size_fft; ++i) {
+            c0[i] = a0[i] * b0[i];
+            c1[i] = a1[i] * b1[i];
+            c2[i] = a2[i] * b2[i];
+        }
+        trans(c0, true), trans(c1, true), trans(c2, true);
+        static const mint mod0 = MOD0, mod01 = mod0 * MOD1;
+        vector<mint> res(N + M - 1);
+        for (int i = 0; i < N + M - 1; ++i) {
+            int y0 = c0[i].val;
+            int y1 = (imod0 * (c1[i] - y0)).val;
+            int y2 = (imod01 * (c2[i] - y0) - imod1 * y1).val;
+            res[i] = mod01 * y2 + mod0 * y1 + y0;
+        }
+        return res;
+    }
+
+    // long long
+    vector<long long> mul_ll
+    (const vector<long long> &A, const vector<long long> &B) {
+        if (A.empty() || B.empty()) return {};
+        int N = (int)A.size(), M = (int)B.size();
+        if (min(N, M) < 30) return naive_mul(A, B);
+        int size_fft = get_fft_size(N, M);
+        vector<mint0> a0(size_fft, 0), b0(size_fft, 0), c0(size_fft, 0);
+        vector<mint1> a1(size_fft, 0), b1(size_fft, 0), c1(size_fft, 0);
+        vector<mint2> a2(size_fft, 0), b2(size_fft, 0), c2(size_fft, 0);
+        for (int i = 0; i < N; ++i)
+            a0[i] = A[i], a1[i] = A[i], a2[i] = A[i];
+        for (int i = 0; i < M; ++i)
+            b0[i] = B[i], b1[i] = B[i], b2[i] = B[i];
+        trans(a0), trans(a1), trans(a2), trans(b0), trans(b1), trans(b2);
+        for (int i = 0; i < size_fft; ++i) {
+            c0[i] = a0[i] * b0[i];
+            c1[i] = a1[i] * b1[i];
+            c2[i] = a2[i] * b2[i];
+        }
+        trans(c0, true), trans(c1, true), trans(c2, true);
+        static const long long mod0 = MOD0, mod01 = mod0 * MOD1;
+        vector<long long> res(N + M - 1);
+        for (int i = 0; i < N + M - 1; ++i) {
+            int y0 = c0[i].val;
+            int y1 = (imod0 * (c1[i] - y0)).val;
+            int y2 = (imod01 * (c2[i] - y0) - imod1 * y1).val;
+            res[i] = mod01 * y2 + mod0 * y1 + y0;
+        }
         return res;
     }
 };
 
-
+const int MOD = 1000000007;
+using mint = Fp<MOD>;
+using namespace NTT;
 
 int main() {
-    int n, k; cin >> n >> k;
-    n /= 2;
-    vector<long long> v(1<<22, 0);
-    for (int i = 0; i < k; ++i) {
-        int d; cin >> d;
-        v[d] = 1;
+    int N, M;
+    cin >> N >> M;
+    vector<mint> a(N), b(M);
+    for (int i = 0; i < N; ++i) cin >> a[i];
+    for (int i = 0; i < M; ++i) cin >> b[i];
+    auto c = mul(a, b);
+    for (int i = 0; i < N + M - 1; ++i) {
+        if (i) cout << " ";
+        cout << c[i];
     }
-    NTT::trans(v);
-    vector<long long> each(1<<22, 1);
-    for (int i = 0; i < each.size(); ++i) {
-        int N = n;
-        while (N > 0) {
-            if (N & 1) each[i] = each[i] * v[i] % NTT::MOD;
-            v[i] = v[i] * v[i] % NTT::MOD;
-            N >>= 1;
-        }
-    }
-    NTT::trans(each, true);
-    
-    long long res = 0;
-    for (int i = 0; i < each.size(); ++i) {
-        res += each[i] * each[i] % NTT::MOD;
-    }
-    res %= NTT::MOD;
-    cout << res << endl;
+    cout << endl;
 }
