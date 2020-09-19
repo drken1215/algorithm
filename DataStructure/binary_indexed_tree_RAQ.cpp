dcdb6148139293d85c1ebeac41d03493b8733b19
Kensuke Otsuki <dr.ken.1215@gmail.com>
2020-09-19 20:45:38 +0900

    modify BIT

--- DataStructure/binary_indexed_tree_RAQ.cpp
+++ DataStructure/binary_indexed_tree_RAQ.cpp
@@ -2,8 +2,8 @@
 // Binary Indexed Tree (「区間加算」「区間和取得」両対応)
 //
 // verified:
-//   AOJ Course DSL_2_B Range Query - Range Sum Query (RSQ)
-//     http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_B&lang=jp
+//   AOJ Course DSL_2_G RSQ and RAQ
+//     http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_G&lang=ja
 //
 
 
@@ -13,35 +13,48 @@ using namespace std;
 
 
 template <class Abel> struct BIT {
+    Abel UNITY_SUM = 0;
     vector<Abel> dat[2];
-    Abel UNITY_SUM = 0;                     // to be set
+
+    // [0, n)
+    BIT(int n, Abel unity = 0) : UNITY_SUM(unity) {
+        init(n);
+    }
+    void init(int n) {
+        for (int iter = 0; iter < 2; ++iter)
+            dat[iter].assign(n + 1, UNITY_SUM);
+    }
     
-    /* [1, n] */
-    BIT(int n) { init(n); }
-    void init(int n) { for (int iter = 0; iter < 2; ++iter) dat[iter].assign(n + 1, UNITY_SUM); }
-    
-    /* a, b are 1-indexed, [a, b) */
+    // [a, b), a and b are 0-indexed
     inline void sub_add(int p, int a, Abel x) {
-        for (int i = a; i < (int)dat[p].size(); i += i & -i)
+        for (int i = a; i < (int)dat[p].size(); i |= i + 1)
             dat[p][i] = dat[p][i] + x;
     }
     inline void add(int a, int b, Abel x) {
-        sub_add(0, a, x * -(a - 1)); sub_add(1, a, x); sub_add(0, b, x * (b - 1)); sub_add(1, b, x * (-1));
+        sub_add(0, a, x * (-a));
+        sub_add(1, a, x);
+        sub_add(0, b, x * b);
+        sub_add(1, b, x * (-1));
     }
     
-    /* a is 1-indexed, [a, b) */
+    // [a, b), a and b are 0-indexed
     inline Abel sub_sum(int p, int a) {
         Abel res = UNITY_SUM;
-        for (int i = a; i > 0; i -= i & -i) res = res + dat[p][i];
+        for (int i = a - 1; i >= 0; i = (i & (i + 1)) - 1)
+            res = res + dat[p][i];
         return res;
     }
     inline Abel sum(int a, int b) {
-        return sub_sum(0, b - 1) + sub_sum(1, b - 1) * (b - 1) - sub_sum(0, a - 1) - sub_sum(1, a - 1) * (a - 1);
+        return sub_sum(0, b)
+            + sub_sum(1, b) * b
+            - sub_sum(0, a)
+            - sub_sum(1, a) * a;
     }
     
-    /* debug */
+    // debug
     void print() {
-        for (int i = 1; i < (int)dat[0].size(); ++i) cout << sum(i, i + 1) << ",";
+        for (int i = 0; i < (int)dat[0].size(); ++i)
+            cout << sum(i, i + 1) << ",";
         cout << endl;
     }
 };
@@ -49,17 +62,20 @@ template <class Abel> struct BIT {
 
 
 int main() {
-    int N, Q; cin >> N >> Q;
+    int N, Q;
+    cin >> N >> Q;
     BIT<long long> bit(N);
     for (int query = 0; query < Q; ++query) {
-        int type; cin >> type;
+        int type, s, t;
+        cin >> type >> s >> t;
+        --s;
         if (type == 0) {
-            int s, t; long long x; cin >> s >> t >> x;
-            bit.add(s, t+1, x);
+            long long x;
+            cin >> x;
+            bit.add(s, t, x);
         }
         else {
-            int s, t; cin >> s >> t;
-            cout << bit.sum(s, t+1) << endl;
+            cout << bit.sum(s, t) << endl;
         }
     }
 }
--- GraphNetworkFlow/max_flow_ford_fulkerson.cpp
+++ GraphNetworkFlow/max_flow_ford_fulkerson.cpp
@@ -48,10 +48,14 @@ template<class FLOWTYPE> struct Graph {
         list[to].push_back(Edge<FLOWTYPE>((int)list[from].size() - 1, to, from, cap));
     }
 
-    /* 
+    /*
     // debug
     friend ostream& operator << (ostream& s, const Graph& G) {
-        s << endl; for (int i = 0; i < G.V; ++i) { s << i << " : " << G.list[i] << endl; }return s;
+        s << endl;
+        for (int i = 0; i < G.size(); ++i) {
+            s << i << " : " << G.list[i] << endl;
+        }
+        return s;
     }
     */
 };
