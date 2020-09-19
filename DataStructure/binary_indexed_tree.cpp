0bbf4f4a880ff4954cbba19ce540de3cbb33f7c7
Kensuke Otsuki <dr.ken.1215@gmail.com>
2020-09-19 20:30:58 +0900

    modify BIT for 0-indexed

--- DataStructure/binary_indexed_tree.cpp
+++ DataStructure/binary_indexed_tree.cpp
@@ -13,47 +13,51 @@ using namespace std;
 
 
 template <class Abel> struct BIT {
-    const Abel UNITY_SUM = 0;                       // to be set
+    Abel UNITY_SUM = 0;
     vector<Abel> dat;
     
-    /* [1, n] */
-    BIT(int n) : dat(n + 1, UNITY_SUM) { }
-    void init(int n) { dat.assign(n + 1, UNITY_SUM); }
+    // [0, n)
+    BIT(int n, Abel unity = 0) : UNITY_SUM(unity), dat(n, unity) { }
+    void init(int n) {
+        dat.assign(n, UNITY_SUM);
+    }
     
-    /* a is 1-indexed */
+    // a is 0-indexed
     inline void add(int a, Abel x) {
-        for (int i = a; i < (int)dat.size(); i += i & -i)
+        for (int i = a; i < (int)dat.size(); i |= i + 1)
             dat[i] = dat[i] + x;
     }
     
-    /* [1, a], a is 1-indexed */
+    // [0, a), a is 0-indexed
     inline Abel sum(int a) {
         Abel res = UNITY_SUM;
-        for (int i = a; i > 0; i -= i & -i)
+        for (int i = a - 1; i >= 0; i = (i & (i + 1)) - 1)
             res = res + dat[i];
         return res;
     }
     
-    /* [a, b), a and b are 1-indexed */
+    // [a, b), a and b are 0-indexed
     inline Abel sum(int a, int b) {
-        return sum(b - 1) - sum(a - 1);
+        return sum(b) - sum(a);
     }
     
-    /* debug */
+    // debug
     void print() {
-        for (int i = 1; i < (int)dat.size(); ++i) cout << sum(i, i + 1) << ",";
+        for (int i = 0; i < (int)dat.size(); ++i)
+            cout << sum(i, i + 1) << ",";
         cout << endl;
     }
 };
-
 
 
 int main() {
     int N, Q; cin >> N >> Q;
     BIT<int> bit(N);
     for (int query = 0; query < Q; ++query) {
-        int com, x, y; cin >> com >> x >> y;
+        int com, x, y;
+        cin >> com >> x >> y;
+        --x;
         if (com == 0) bit.add(x, y);
-        else cout << bit.sum(x, y+1) << endl;
+        else cout << bit.sum(x, y) << endl;
     }
 }
--- DataStructure/binary_search_on_BIT.cpp
+++ DataStructure/binary_search_on_BIT.cpp
@@ -17,53 +17,56 @@ using namespace std;
 
 // get(k): binary search on BIT
 template <class Abel> struct BIT {
-    const Abel UNITY_SUM = 0;						// to be set
+    Abel UNITY_SUM = 0;
     vector<Abel> dat;
     
-    /* [1, n] */
-    BIT(int n) : dat(n + 1, UNITY_SUM) { }
-    void init(int n) { dat.sssign(n + 1, UNITY_SUM); }
+    // [0, n)
+    BIT(int n, Abel unity = 0) : UNITY_SUM(unity), dat(n, unity) { }
+    void init(int n) {
+        dat.assign(n, UNITY_SUM);
+    }
     
-    /* a is 1-indexed */
+    // a is 0-indexed
     inline void add(int a, Abel x) {
-        for (int i = a; i < (int)dat.size(); i += i & -i)
+        for (int i = a; i < (int)dat.size(); i |= i + 1)
             dat[i] = dat[i] + x;
     }
     
-    /* [1, a], a is 1-indexed */
+    // [0, a), a is 0-indexed
     inline Abel sum(int a) {
         Abel res = UNITY_SUM;
-        for (int i = a; i > 0; i -= i & -i)
+        for (int i = a - 1; i >= 0; i = (i & (i + 1)) - 1)
             res = res + dat[i];
         return res;
     }
     
-    /* [a, b), a and b are 1-indexed */
+    // [a, b), a and b are 0-indexed
     inline Abel sum(int a, int b) {
-        return sum(b - 1) - sum(a - 1);
+        return sum(b) - sum(a);
     }
 
-    /* k-th number (k is 0-indexed) */
+    // k-th number (k is 0-indexed)
     int get(long long k) {
         ++k;
         int res = 0;
-        int N = 1; while (N < (int)dat.size()) N *= 2;
+        int N = 1;
+        while (N < (int)dat.size()) N *= 2;
         for (int i = N / 2; i > 0; i /= 2) {
-            if (res + i < (int)dat.size() && dat[res + i] < k) {
-                k = k - dat[res + i];
+            if (res + i - 1 < (int)dat.size() && dat[res + i - 1] < k) {
+                k = k - dat[res + i - 1];
                 res = res + i;
             }
         }
-        return res + 1;
+        return res;
     }
     
-    /* debug */
+    // debug
     void print() {
-        for (int i = 1; i < (int)dat.size(); ++i) cout << sum(i, i + 1) << ",";
+        for (int i = 0; i < (int)dat.size(); ++i)
+            cout << sum(i, i + 1) << ",";
         cout << endl;
     }
 };
-
 
 
 int main() {
