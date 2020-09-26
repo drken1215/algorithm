d66ad250ecfc0b8661a9f415915a4183126936d8
Kensuke Otsuki <dr.ken.1215@gmail.com>
2020-09-27 02:57:20 +0900

    modify lazy segment tree

--- DataStructure/segment_tree_delay.cpp
+++ DataStructure/segment_tree_delay.cpp
@@ -5,6 +5,9 @@
 //   AOJ Course Range Query - RMQ and RAQ
 //     http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_H&lang=jp
 //
+//   AtCoder ACL Beginner Contest E - Replace Digits
+//     https://atcoder.jp/contests/abl/tasks/abl_e
+//
 
 
 /*
@@ -12,12 +15,12 @@
       FM(a, b): 2 つのモノイド間に定義される演算
       FA(a, d): モノイド元 a への作用素 d による作用
       FL(d, e): 作用素 d への作用素 e による作用
-      UNITY_MONOID: モノイドの単位元
-      UNITY_LAZY: 作用素の単位元
+      IDENTITY_MONOID: モノイドの単位元
+      IDENTITY_LAZY: 作用素の単位元
 
 
     // Construction
-    SegTree(n, fm, fa, fl, unity_monoid, unity_lazy)
+    SegTree(n, fm, fa, fl, identity_monoid, identity_lazy)
       ex: starry sky tree (区間加算、区間min取得)
         auto fm = [](long long a, long long b) { return min(a, b); };
         auto fa = [](long long &a, long long d) { a += d; };
@@ -29,17 +32,15 @@
     init(n): サイズ n に初期化
     set(a, v): a 番目の値を v にセットする
     build(): set した値を元にセグメントツリー全体を構築する、O(n)
-    
 
+    
     // Queries
     update(a, b, v): 区間 [a, b) を作用素 v を用いて更新する, O(log n)
     get(a, b): 区間 [a, b) についての演算結果を返す, O(log n)
 */
 
 
-#include <iostream>
-#include <vector>
-#include <functional>
+#include <bits/stdc++.h>
 using namespace std;
 
 
@@ -51,97 +52,192 @@ template<class Monoid, class Action> struct SegTree {
     FuncMonoid FM;
     FuncAction FA;
     FuncLazy FL;
-    Monoid UNITY_MONOID;
-    Action UNITY_LAZY;
+    Monoid IDENTITY_MONOID;
+    Action IDENTITY_LAZY;
     int SIZE, HEIGHT;
     vector<Monoid> dat;
     vector<Action> lazy;
     
     SegTree() { }
     SegTree(int n, const FuncMonoid fm, const FuncAction fa, const FuncLazy fl,
-            const Monoid &unity_monoid, const Action &unity_lazy)
-    : FM(fm), FA(fa), FL(fl), UNITY_MONOID(unity_monoid), UNITY_LAZY(unity_lazy) {
-        SIZE = 1; HEIGHT = 0;
+            const Monoid &identity_monoid, const Action &identity_lazy)
+    : FM(fm), FA(fa), FL(fl), 
+      IDENTITY_MONOID(identity_monoid), IDENTITY_LAZY(identity_lazy) {
+        SIZE = 1, HEIGHT = 0;
         while (SIZE < n) SIZE <<= 1, ++HEIGHT;
-        dat.assign(SIZE * 2, UNITY_MONOID);
-        lazy.assign(SIZE * 2, UNITY_LAZY);
+        dat.assign(SIZE * 2, IDENTITY_MONOID);
+        lazy.assign(SIZE * 2, IDENTITY_LAZY);
     }
     void init(int n, const FuncMonoid fm, const FuncAction fa, const FuncLazy fl,
-              const Monoid &unity_monoid, const Action &unity_lazy) {
-        FM = fm; FA = fa; FL = fl;
-        UNITY_MONOID = unity_monoid; UNITY_LAZY = unity_lazy;
+              const Monoid &identity_monoid, const Action &identity_lazy) {
+        FM = fm, FA = fa, FL = fl;
+        IDENTITY_MONOID = identity_monoid, IDENTITY_LAZY = identity_lazy;
         SIZE = 1; HEIGHT = 0;
         while (SIZE < n) SIZE <<= 1, ++HEIGHT;
-        dat.assign(SIZE * 2, UNITY_MONOID);
-        lazy.assign(SIZE * 2, UNITY_LAZY);
+        dat.assign(SIZE * 2, IDENTITY_MONOID);
+        lazy.assign(SIZE * 2, IDENTITY_LAZY);
     }
     
-    /* set, a is 0-indexed */
-    void set(int a, const Monoid &v) { dat[a + SIZE] = v; }
+    // set, a is 0-indexed
+    void set(int a, const Monoid &v) {
+        dat[a + SIZE] = v;
+    }
     void build() {
         for (int k = SIZE - 1; k > 0; --k)
             dat[k] = FM(dat[k*2], dat[k*2+1]);
     }
     
-    /* update [a, b) */
+    // update [a, b)
     inline void evaluate(int k) {
-        if (lazy[k] == UNITY_LAZY) return;
+        if (lazy[k] == IDENTITY_LAZY) return;
         if (k < SIZE) FL(lazy[k*2], lazy[k]), FL(lazy[k*2+1], lazy[k]);
         FA(dat[k], lazy[k]);
-        lazy[k] = UNITY_LAZY;
+        lazy[k] = IDENTITY_LAZY;
     }
     inline void update(int a, int b, const Action &v, int k, int l, int r) {
         evaluate(k);
-        if (a <= l && r <= b)  FL(lazy[k], v), evaluate(k);
+        if (a <= l && r <= b) FL(lazy[k], v), evaluate(k);
         else if (a < r && l < b) {
-            update(a, b, v, k*2, l, (l+r)>>1), update(a, b, v, k*2+1, (l+r)>>1, r);
+            update(a, b, v, k*2, l, (l+r)>>1);
+            update(a, b, v, k*2+1, (l+r)>>1, r);
             dat[k] = FM(dat[k*2], dat[k*2+1]);
         }
     }
-    inline void update(int a, int b, const Action &v) { update(a, b, v, 1, 0, SIZE); }
+    inline void update(int a, int b, const Action &v) { 
+        update(a, b, v, 1, 0, SIZE);
+    }
     
-    /* get [a, b) */
+    // get [a, b)
     inline Monoid get(int a, int b, int k, int l, int r) {
         evaluate(k);
         if (a <= l && r <= b)
             return dat[k];
         else if (a < r && l < b)
-            return FM(get(a, b, k*2, l, (l+r)>>1), get(a, b, k*2+1, (l+r)>>1, r));
+            return FM(get(a, b, k*2, l, (l+r)>>1), 
+                      get(a, b, k*2+1, (l+r)>>1, r));
         else
-            return UNITY_MONOID;
+            return IDENTITY_MONOID;
     }
-    inline Monoid get(int a, int b) { return get(a, b, 1, 0, SIZE); }
-    inline Monoid operator [] (int a) { return get(a, a+1); }
+    inline Monoid get(int a, int b) { 
+        return get(a, b, 1, 0, SIZE);
+    }
+    inline Monoid operator [] (int a) {
+        return get(a, a + 1);
+    }
     
-    /* debug */
+    // debug
     void print() {
-        for (int i = 0; i < SIZE; ++i) { cout << (*this)[i]; if (i != SIZE) cout << ","; }
+        for (int i = 0; i < SIZE; ++i) {
+            if (i) cout << ",";
+            cout << (*this)[i];
+        }
         cout << endl;
     }
 };
 
 
-int main() {
-    int N, Q; scanf("%d %d", &N, &Q);
-    auto fm = [](long long a, long long b) { return min(a, b); };
-    auto fa = [](long long &a, long long d) { a += d; };
-    auto fl = [](long long &d, long long e) { d += e; };
-    SegTree<long long, long long> seg(N, fm, fa, fl, (1LL<<60), 0);
+// modint
+template<int MOD> struct Fp {
+    long long val;
+    constexpr Fp(long long v = 0) noexcept : val(v % MOD) {
+        if (val < 0) val += MOD;
+    }
+    constexpr int getmod() const { return MOD; }
+    constexpr Fp operator - () const noexcept {
+        return val ? MOD - val : 0;
+    }
+    constexpr Fp operator + (const Fp& r) const noexcept { return Fp(*this) += r; }
+    constexpr Fp operator - (const Fp& r) const noexcept { return Fp(*this) -= r; }
+    constexpr Fp operator * (const Fp& r) const noexcept { return Fp(*this) *= r; }
+    constexpr Fp operator / (const Fp& r) const noexcept { return Fp(*this) /= r; }
+    constexpr Fp& operator += (const Fp& r) noexcept {
+        val += r.val;
+        if (val >= MOD) val -= MOD;
+        return *this;
+    }
+    constexpr Fp& operator -= (const Fp& r) noexcept {
+        val -= r.val;
+        if (val < 0) val += MOD;
+        return *this;
+    }
+    constexpr Fp& operator *= (const Fp& r) noexcept {
+        val = val * r.val % MOD;
+        return *this;
+    }
+    constexpr Fp& operator /= (const Fp& r) noexcept {
+        long long a = r.val, b = MOD, u = 1, v = 0;
+        while (b) {
+            long long t = a / b;
+            a -= t * b, swap(a, b);
+            u -= t * v, swap(u, v);
+        }
+        val = val * u % MOD;
+        if (val < 0) val += MOD;
+        return *this;
+    }
+    constexpr bool operator == (const Fp& r) const noexcept {
+        return this->val == r.val;
+    }
+    constexpr bool operator != (const Fp& r) const noexcept {
+        return this->val != r.val;
+    }
+    friend constexpr istream& operator >> (istream &is, Fp<MOD>& x) noexcept {
+        is >> x.val;
+        x.val %= MOD;
+        if (x.val < 0) x.val += MOD;
+        return is;
+    }
+    friend constexpr ostream& operator << (ostream &os, const Fp<MOD>& x) noexcept {
+        return os << x.val;
+    }
+    friend constexpr Fp<MOD> modpow(const Fp<MOD> &a, long long n) noexcept {
+        if (n == 0) return 1;
+        auto t = modpow(a, n / 2);
+        t = t * t;
+        if (n & 1) t = t * a;
+        return t;
+    }
+};
 
-    // 0 で初期化
-    for (int i = 0; i < N; ++i) seg.set(i, 0);
+const int MOD = 998244353;
+using mint = Fp<MOD>;
+using pll = pair<mint,int>; // val, num
+
+
+int main() {
+    int N, Q;
+    cin >> N >> Q;
+    vector<mint> ten(N, 1), sum(N+1, 0);
+    for (int i = 1; i < N; ++i) ten[i] = ten[i-1] * 10;
+    for (int i = 0; i < N; ++i) sum[i+1] = sum[i] + ten[i];
+
+    // define segtree
+    auto fm = [&](pll a, pll b) {
+        mint first = a.first * ten[b.second] + b.first;
+        int second = a.second + b.second;
+        return pll(first, second);
+    };
+    auto fa = [&](pll &a, int d) {
+        if (d == 0) return;
+        a.first = sum[a.second] * d;
+    };
+    auto fl = [&](int &d, int e) {
+        d = e;
+    };
+    pll identity_monoid = pll(mint(0), 0);
+    int identity_lazy = 0;
+    SegTree<pll, int> seg(N, fm, fa, fl, identity_monoid, identity_lazy);
+
+    // initialization
+    for (int i = 0; i < N; ++i) seg.set(i, pll(mint(1), 1));
     seg.build();
 
-    // 各クエリ
-    for (int q = 0; q < Q; ++q) {
-        int type, a, b; scanf("%d %d %d", &type, &a, &b);
-        if (type == 0) {
-            long long v; scanf("%lld", &v);
-            seg.update(a, b+1, v);
-        }
-        else {
-            cout << seg.get(a, b+1) << endl;
-        }
-        //seg.print();
+    // query
+    while (Q--) {
+        int l, r, d;
+        cin >> l >> r >> d;
+        --l;
+        seg.update(l, r, d);
+        cout << seg.get(0, N).first << endl;
     }
 }
