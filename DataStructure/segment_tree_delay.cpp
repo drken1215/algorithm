c097069d433f858db5412878a8243e26973739d9
Kensuke Otsuki <dr.ken.1215@gmail.com>
2020-09-27 03:50:32 +0900

    modify lazy segment tree

--- DataStructure/segment_tree_delay.cpp
+++ DataStructure/segment_tree_delay.cpp
@@ -14,18 +14,18 @@
     セグメントツリーは「作用つきモノイド」上で定義される
       FM(a, b): 2 つのモノイド間に定義される演算
       FA(a, d): モノイド元 a への作用素 d による作用
-      FL(d, e): 作用素 d への作用素 e による作用
+      FC(d, e): 作用素 d への作用素 e の合成
       IDENTITY_MONOID: モノイドの単位元
       IDENTITY_LAZY: 作用素の単位元
 
 
     // Construction
-    SegTree(n, fm, fa, fl, identity_monoid, identity_lazy)
+    SegTree(N, fm, fa, fc, identity_monoid, identity_lazy)
       ex: starry sky tree (区間加算、区間min取得)
         auto fm = [](long long a, long long b) { return min(a, b); };
         auto fa = [](long long &a, long long d) { a += d; };
-        auto fl = [](long long &d, long long e) { d += e; };
-        SegTree<long long, long long> seg(N, fm, fa, fl, (1LL<<60), 0);
+        auto fc = [](long long &d, long long e) { d += e; };
+        SegTree<long long, long long> seg(N, fm, fa, fc, (1LL<<60), 0);
 
 
     // Initialization
@@ -33,7 +33,7 @@
     set(a, v): a 番目の値を v にセットする
     build(): set した値を元にセグメントツリー全体を構築する、O(n)
 
-    
+
     // Queries
     update(a, b, v): 区間 [a, b) を作用素 v を用いて更新する, O(log n)
     get(a, b): 区間 [a, b) についての演算結果を返す, O(log n)
@@ -42,46 +42,49 @@
 
 #include <bits/stdc++.h>
 using namespace std;
+template<class T1, class T2> ostream& operator << 
+(ostream &s, pair<T1,T2> P)
+{ return s << '<' << P.first << ", " << P.second << '>'; }
 
 
 // Segment Tree
 template<class Monoid, class Action> struct SegTree {
     using FuncMonoid = function< Monoid(Monoid, Monoid) >;
     using FuncAction = function< void(Monoid&, Action) >;
-    using FuncLazy = function< void(Action&, Action) >;
+    using FuncComposition = function< void(Action&, Action) >;
     FuncMonoid FM;
     FuncAction FA;
-    FuncLazy FL;
+    FuncComposition FC;
     Monoid IDENTITY_MONOID;
     Action IDENTITY_LAZY;
     int SIZE, HEIGHT;
     vector<Monoid> dat;
     vector<Action> lazy;
     
-    SegTree() { }
-    SegTree(int n, const FuncMonoid fm, const FuncAction fa, const FuncLazy fl,
+    SegTree() {}
+    SegTree(int n, const FuncMonoid fm, const FuncAction fa,
+            const FuncComposition fc,
             const Monoid &identity_monoid, const Action &identity_lazy)
-    : FM(fm), FA(fa), FL(fl), 
+    : FM(fm), FA(fa), FC(fc), 
       IDENTITY_MONOID(identity_monoid), IDENTITY_LAZY(identity_lazy) {
         SIZE = 1, HEIGHT = 0;
         while (SIZE < n) SIZE <<= 1, ++HEIGHT;
         dat.assign(SIZE * 2, IDENTITY_MONOID);
         lazy.assign(SIZE * 2, IDENTITY_LAZY);
     }
-    void init(int n, const FuncMonoid fm, const FuncAction fa, const FuncLazy fl,
+    void init(int n, const FuncMonoid fm, const FuncAction fa,
+              const FuncComposition fc,
               const Monoid &identity_monoid, const Action &identity_lazy) {
-        FM = fm, FA = fa, FL = fl;
+        FM = fm, FA = fa, FC = fc;
         IDENTITY_MONOID = identity_monoid, IDENTITY_LAZY = identity_lazy;
-        SIZE = 1; HEIGHT = 0;
+        SIZE = 1, HEIGHT = 0;
         while (SIZE < n) SIZE <<= 1, ++HEIGHT;
         dat.assign(SIZE * 2, IDENTITY_MONOID);
         lazy.assign(SIZE * 2, IDENTITY_LAZY);
     }
     
     // set, a is 0-indexed
-    void set(int a, const Monoid &v) {
-        dat[a + SIZE] = v;
-    }
+    void set(int a, const Monoid &v) { dat[a + SIZE] = v; }
     void build() {
         for (int k = SIZE - 1; k > 0; --k)
             dat[k] = FM(dat[k*2], dat[k*2+1]);
@@ -90,13 +93,13 @@ template<class Monoid, class Action> struct SegTree {
     // update [a, b)
     inline void evaluate(int k) {
         if (lazy[k] == IDENTITY_LAZY) return;
-        if (k < SIZE) FL(lazy[k*2], lazy[k]), FL(lazy[k*2+1], lazy[k]);
+        if (k < SIZE) FC(lazy[k*2], lazy[k]), FC(lazy[k*2+1], lazy[k]);
         FA(dat[k], lazy[k]);
         lazy[k] = IDENTITY_LAZY;
     }
     inline void update(int a, int b, const Action &v, int k, int l, int r) {
         evaluate(k);
-        if (a <= l && r <= b) FL(lazy[k], v), evaluate(k);
+        if (a <= l && r <= b) FC(lazy[k], v), evaluate(k);
         else if (a < r && l < b) {
             update(a, b, v, k*2, l, (l+r)>>1);
             update(a, b, v, k*2+1, (l+r)>>1, r);
@@ -129,7 +132,7 @@ template<class Monoid, class Action> struct SegTree {
     void print() {
         for (int i = 0; i < SIZE; ++i) {
             if (i) cout << ",";
-            cout << (*this)[i];
+            cout << get(i, i+1);
         }
         cout << endl;
     }
@@ -203,7 +206,6 @@ const int MOD = 998244353;
 using mint = Fp<MOD>;
 using pll = pair<mint,int>; // val, num
 
-
 int main() {
     int N, Q;
     cin >> N >> Q;
@@ -221,15 +223,15 @@ int main() {
         if (d == 0) return;
         a.first = sum[a.second] * d;
     };
-    auto fl = [&](int &d, int e) {
+    auto fc = [&](int &d, int e) {
         d = e;
     };
-    pll identity_monoid = pll(mint(0), 0);
+    pll identity_monoid = pll(mint(0LL), 0);
     int identity_lazy = 0;
-    SegTree<pll, int> seg(N, fm, fa, fl, identity_monoid, identity_lazy);
+    SegTree<pll, int> seg(N, fm, fa, fc, identity_monoid, identity_lazy);
 
     // initialization
-    for (int i = 0; i < N; ++i) seg.set(i, pll(mint(1), 1));
+    for (int i = 0; i < N; ++i) seg.set(i, pll(mint(1LL), 1));
     seg.build();
 
     // query
@@ -239,5 +241,7 @@ int main() {
         --l;
         seg.update(l, r, d);
         cout << seg.get(0, N).first << endl;
+
+        //seg.print();
     }
 }
