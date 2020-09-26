ac00521a2ae6d3f9d59e95f21eb97aa0e13d8688
Kensuke Otsuki <dr.ken.1215@gmail.com>
2020-09-26 20:26:48 +0900

    modify BIT2D for 0-indexed

--- DataStructure/binary_indexed_tree_2D_RAQ.cpp
+++ DataStructure/binary_indexed_tree_2D_RAQ.cpp
@@ -13,54 +13,61 @@ using namespace std;
 
 
 template <class Abel> struct BIT2D {
-    const Abel UNITY_SUM = 0;						// to be set
+    const Abel UNITY_SUM = 0;
     vector<vector<Abel> > dat[2][2];
     
-    BIT2D(int n = 1, int m = 1) { init(n, m); }
-    void init(int n = 1, int m = 1) {
-        for (int i = 0; i < 2; ++i) for (int j = 0; j < 2; ++j) dat[i][j].resize(n+1, vector<Abel>(m+1, UNITY_SUM));
+    BIT2D(int n, int m, Abel unity = 0) : UNITY_SUM(unity) {
+        init(n, m);
+    }
+    void init(int n, int m) {
+        for (int i = 0; i < 2; ++i)
+            for (int j = 0; j < 2; ++j)
+                dat[i][j].assign(n + 1, vector<Abel>(m + 1, UNITY_SUM));
     }
     
-    /* a1 <= x < b1, a2 <= y < b2, 1-indexd */
-    inline void subsub_add(int f, int s, int a, int b, Abel x) {
-        for (int i = a; i < (int)dat[f][s].size(); i += i & -i)
-            for (int j = b; j < (int)dat[f][s][0].size(); j += j & -j)
-                dat[f][s][i][j] = dat[f][s][i][j] + x;
+    // x1 <= x < x2, y1 <= y < y2, 0-indexed
+    inline void subsub_add(int f, int s, int x, int y, Abel v) {
+        for (int i = x; i < (int)dat[f][s].size(); i |= i + 1)
+            for (int j = y; j < (int)dat[f][s][0].size(); j |= j + 1)
+                dat[f][s][i][j] = dat[f][s][i][j] + v;
     }
-    inline void sub_add(int a1, int a2, Abel x) {
-        subsub_add(0, 0, a1, a2, x*(a1-1)*(a2-1));
-        subsub_add(1, 0, a1, a2, -x*(a1-1));
-        subsub_add(0, 1, a1, a2, -x*(a2-1));
-        subsub_add(1, 1, a1, a2, x);
+    inline void sub_add(int x, int y, Abel v) {
+        subsub_add(0, 0, x, y, v * x * y);
+        subsub_add(1, 0, x, y, -v * x);
+        subsub_add(0, 1, x, y, -v * y);
+        subsub_add(1, 1, x, y, v);
     }
-    inline void add(int a1, int a2, int b1, int b2, Abel x) {
-        sub_add(a1, a2, x);
-        sub_add(a1, b2, -x);
-        sub_add(b1, a2, -x);
-        sub_add(b1, b2, x);
+    inline void add(int x1, int x2, int y1, int y2, Abel v) {
+        sub_add(x1, y1, v);
+        sub_add(x1, y2, -v);
+        sub_add(x2, y1, -v);
+        sub_add(x2, y2, v);
     }
     
-    /* a1 <= x < b1, a2 <= y < b2, 1-indexd */
-    inline Abel subsub_sum(int f, int s, int a, int b) {
+    // x1 <= x < x2, y1 <= y < y2, 0-indexed
+    inline Abel subsub_sum(int f, int s, int x, int y) {
         Abel res = 0;
-        for (int i = a; i > 0; i -= i & -i)
-            for (int j = b; j > 0; j -= j & -j)
+        for (int i = x; i > 0; i = (i & (i + 1)) - 1)
+            for (int j = y; j > 0; j = (j & (j + 1)) - 1)
                 res = res + dat[f][s][i][j];
         return res;
     }
-    inline Abel sub_sum(int a1, int a2) {
+    inline Abel sub_sum(int x, int y) {
         Abel res = 0;
-        res += subsub_sum(0, 0, a1-1, a2-1);
-        res += subsub_sum(1, 0, a1-1, a2-1) * (a2-1);
-        res += subsub_sum(0, 1, a1-1, a2-1) * (a1-1);
-        res += subsub_sum(1, 1, a1-1, a2-1) * (a1-1) * (a2-1);
+        res += subsub_sum(0, 0, x, y);
+        res += subsub_sum(1, 0, x, y) * y;
+        res += subsub_sum(0, 1, x, y) * x;
+        res += subsub_sum(1, 1, x, y) * x * y;
         return res;
     }
-    inline Abel sum(int a1, int a2, int b1, int b2) {
-        return sub_sum(b1, b2) - sub_sum(a1, b2) - sub_sum(b1, a2) + sub_sum(a1, a2);
+    inline Abel sum(int x1, int x2, int y1, int y2) {
+        return sub_sum(x2, y2)
+            - sub_sum(x1, y2)
+            - sub_sum(x2, y1)
+            + sub_sum(x1, y1);
     }
     
-    /* debug */
+    // debug
     void print() {
         for (int i = 1; i < (int)dat.size(); ++i) {
             for (int j = 1; j < (int)dat[0].size(); ++j)
@@ -72,7 +79,7 @@ template <class Abel> struct BIT2D {
 
 
 
-/* xor に対応するため */
+// xor に対応するため
 struct XOR {
     long long val;
     
@@ -95,18 +102,18 @@ int main() {
     int n, m;
     int q, x0, y0, x1, y1;
     long long v;
-    
-    cin >> n >> m;
+
+    scanf("%d %d", &n, &m);
     BIT2D<XOR> bit(n, n);
     for (int i = 0; i < m; ++i) {
         scanf("%d", &q);
         if (q == 2) {
             scanf("%d %d %d %d %lld", &x0, &y0, &x1, &y1, &v);
-            bit.add(x0, y0, x1+1, y1+1, v);
+            bit.add(x0 - 1, x1, y0 - 1, y1, v);
         }
         if (q == 1) {
             scanf("%d %d %d %d", &x0, &y0, &x1, &y1);
-            cout << bit.sum(x0, y0, x1+1, y1+1).val << endl;
+            printf("%d\n", bit.sum(x0 - 1, x1, y0 - 1, y1).val);
         }
     }
 }
