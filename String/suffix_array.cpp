f437fba8cba4b5d2fc1ebfef9c60e7728a43a36a
drken1215 <dr.ken.1215@gmail.com>
2022-12-16 05:28:32 +0900

    add lower_bound for Suffix Array

--- String/suffix_array.cpp
+++ String/suffix_array.cpp
@@ -1,9 +1,13 @@
 //
 // RMQ (by sparse table)
 //
-// verified (suffix array の lcp を sparse table で求める):
+// verified (suffix array の lcp を sparse table で求める)
+//
 //   ARC 060 F - 最良表現
 //     https://beta.atcoder.jp/contests/arc060/tasks/arc060_d
+//
+//   AOJ 2644 Longest Match
+//     https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=2644
 //
 
 
@@ -38,94 +42,227 @@ template<class MeetSemiLattice> struct SparseTable {
     }
 };
 
+// SA-IS (O(N))
+template<class Str> struct SuffixArray {
+    // data
+    Str str;
+    vector<int> sa;    // sa[i] : the starting index of the i-th smallest suffix (i = 0, 1, ..., n)
+    vector<int> rank;  // rank[sa[i]] = i
+    vector<int> lcp;   // lcp[i]: the lcp of sa[i] and sa[i+1] (i = 0, 1, ..., n-1)
+    SparseTable<int> st;  // use for calcultating lcp(i, j)
 
-// Suffix Array ( Manber&Myers: O(n (logn)^2) )
-struct SuffixArray {
-    string str;
-    vector<int> sa;         // sa[i] : the starting index of the i-th smallest suffix (i = 0, 1, ..., n)
-    vector<int> lcp;        // lcp[i]: the lcp of sa[i] and sa[i+1] (i = 0, 1, ..., n-1)
-    inline int& operator [] (int i) {return sa[i];}
-    
-    SuffixArray(const string& str_) : str(str_) { buildSA(); calcLCP(); }
-    void init(const string& str_) { str = str_; buildSA(); calcLCP(); }
-    
-    // build SA
-    vector<int> rank_sa, tmp_rank_sa;
-    struct CompareSA {
-        int n, k;
-        const vector<int> &rank;
-        CompareSA(int n, int k, const vector<int> &rank_sa) : n(n), k(k), rank(rank_sa) {}
-        bool operator()(int i, int j) {
-            if (rank[i] != rank[j]) return (rank[i] < rank[j]);
-            else {
-                int rank_ik = (i + k <= n ? rank[i + k] : -1);
-                int rank_jk = (j + k <= n ? rank[j + k] : -1);
-                return (rank_ik < rank_jk);
+    // getter
+    int& operator [] (int i) {
+        return sa[i];
+    }
+    vector<int> get_sa() { return sa; }
+    vector<int> get_rank() { return rank; }
+    vector<int> get_lcp() { return lcp; }
+
+    // constructor
+    SuffixArray(const Str& str_) : str(str_) {
+        build_sa();
+    }
+    void init(const Str& str_) {
+        str = str_;
+        build_sa();
+    }
+    void build_sa() {
+        vector<int> s;
+        for (int i = 0; i < (int)str.size(); ++i) {
+            s.push_back(str[i] + 1);
+        }
+        s.push_back(0);
+        sa = sa_is(s);
+        calcLCP(s);
+        buildSparseTable();
+    }
+
+    // SA-IS
+    // upper: # of characters 
+    vector<int> sa_is(vector<int> &s, int upper = 256) {
+        int N = (int)s.size();
+        if (N == 0) return {};
+        else if (N == 1) return {0};
+        else if (N == 2) {
+            if (s[0] < s[1]) return {0, 1};
+            else return {1, 0};
+        }
+
+        vector<int> isa(N);
+        vector<bool> ls(N, false);
+        for (int i = N - 2; i >= 0; --i) {
+            ls[i] = (s[i] == s[i + 1]) ? ls[i + 1] : (s[i] < s[i + 1]);
+        }
+        vector<int> sum_l(upper + 1, 0), sum_s(upper + 1, 0);
+        for (int i = 0; i < N; ++i) {
+            if (!ls[i]) ++sum_s[s[i]];
+            else ++sum_l[s[i] + 1];
+        }
+        for (int i = 0; i <= upper; ++i) {
+            sum_s[i] += sum_l[i];
+            if (i < upper) sum_l[i + 1] += sum_s[i];
+        }
+
+        auto induce = [&](const vector<int> &lms) -> void {
+            fill(isa.begin(), isa.end(), -1);
+            vector<int> buf(upper + 1);
+            copy(sum_s.begin(), sum_s.end(), buf.begin());
+            for (auto d: lms) {
+                if (d == N) continue;
+                isa[buf[s[d]]++] = d;
+            }
+            copy(sum_l.begin(), sum_l.end(), buf.begin());
+            isa[buf[s[N - 1]]++] = N - 1;
+            for (int i = 0; i < N; ++i) {
+                int v = isa[i];
+                if (v >= 1 && !ls[v - 1]) {
+                    isa[buf[s[v - 1]]++] = v - 1;
+                }
+            }
+            copy(sum_l.begin(), sum_l.end(), buf.begin());
+            for (int i = N - 1; i >= 0; --i) {
+                int v = isa[i];
+                if (v >= 1 && ls[v - 1]) {
+                    isa[--buf[s[v - 1] + 1]] = v - 1;
+                }
+            }
+        };
+            
+        vector<int> lms, lms_map(N + 1, -1);
+        int M = 0;
+        for (int i = 1; i < N; ++i) {
+            if (!ls[i - 1] && ls[i]) {
+                lms_map[i] = M++;
             }
         }
-    };
-    void buildSA() {
-        int n = (int)str.size();
-        sa.resize(n+1), lcp.resize(n+1), rank_sa.resize(n+1), tmp_rank_sa.resize(n+1);
-        for (int i = 0; i < n; ++i) sa[i] = i, rank_sa[i] = (int)str[i];
-        sa[n] = n, rank_sa[n] = -1;
-        for (int k = 1; k <= n; k *= 2) {
-            CompareSA csa(n, k, rank_sa);
-            sort(sa.begin(), sa.end(), csa);
-            tmp_rank_sa[sa[0]] = 0;
-            for (int i = 1; i <= n; ++i) {
-                tmp_rank_sa[sa[i]] = tmp_rank_sa[sa[i - 1]];
-                if (csa(sa[i - 1], sa[i])) ++tmp_rank_sa[sa[i]];
+        lms.reserve(M);
+        for (int i = 1; i < N; ++i) {
+            if (!ls[i - 1] && ls[i]) {
+                lms.push_back(i);
             }
-            for (int i = 0; i <= n; ++i) rank_sa[i] = tmp_rank_sa[i];
+        }
+        induce(lms);
+
+        if (M) {
+            vector<int> lms2;
+            lms2.reserve(isa.size());
+            for (auto v: isa) {
+                if (lms_map[v] != -1) lms2.push_back(v);
+            }
+            int rec_upper = 0;
+            vector<int> rec_s(M);
+            rec_s[lms_map[lms2[0]]] = 0;
+            for (int i = 1; i < M; ++i) {
+                int l = lms2[i - 1], r = lms2[i];
+                int nl = (lms_map[l] + 1 < M) ? lms[lms_map[l] + 1] : N;
+                int nr = (lms_map[r] + 1 < M) ? lms[lms_map[r] + 1] : N;
+                bool same = true;
+                if (nl - l != nr - r) same = false;
+                else {
+                    while (l < nl) {
+                        if (s[l] != s[r]) break;
+                        ++l, ++r;
+                    }
+                    if (l == N || s[l] != s[r]) same = false;
+                }
+                if (!same) ++rec_upper;
+                rec_s[lms_map[lms2[i]]] = rec_upper;
+            }
+            auto rec_sa = sa_is(rec_s, rec_upper);
+
+            vector<int> sorted_lms(M);
+            for (int i = 0; i < M; ++i) {
+                sorted_lms[i] = lms[rec_sa[i]];
+            }
+            induce(sorted_lms);
+        }
+        return isa;
+    }
+
+    // find min id that str.substr(sa[id]) >= T
+    int lower_bound(const string& T) {
+        int left = -1, right = sa.size();
+        while (right - left > 1) {
+            int mid = (left + right) / 2;
+            if (str.compare(sa[mid], string::npos, T) < 0)
+                left = mid;
+            else
+                right = mid;
+        }
+        return right;
+    }
+
+    // find min id that str.substr(sa[id], T.size()) > T
+    int upper_bound(const string& T) {
+        int left = -1, right = sa.size();
+        while (right - left > 1) {
+            int mid = (left + right) / 2;
+            if (str.compare(sa[mid], T.size(), T) <= 0)
+                left = mid;
+            else
+                right = mid;
+        }
+        return right;
+    }
+
+    // search
+    bool is_contain(const string& T) {
+        int lb = lower_bound(T);
+        if (lb >= sa.size()) return false;
+        return str.compare(sa[lb], T.size(), T) == 0;
+    }
+
+    // find lcp
+    void calcLCP(const vector<int> &s) {
+        int N = (int)s.size();
+        rank.assign(N, 0), lcp.assign(N, 0);
+        for (int i = 0; i < N; ++i) rank[sa[i]] = i;
+        int h = 0;
+        for (int i = 0; i < N - 1; ++i) {
+            int pi = sa[rank[i] - 1];
+            if (h > 0) --h;
+            for (; pi + h < N && i + h < N; ++h) {
+                if (s[pi + h] != s[i + h]) break;
+            }
+            lcp[rank[i] - 1] = h;
         }
     }
-    vector<int> rsa;
-    SparseTable<int> st;
-    void calcLCP() {
-        int n = (int)str.size();
-        rsa.resize(n+1);
-        for (int i = 0; i <= n; ++i) rsa[sa[i]] = i;
-        lcp.resize(n+1);
-        lcp[0] = 0;
-        int cur = 0;
-        for (int i = 0; i < n; ++i) {
-            int pi = sa[rsa[i] - 1];
-            if (cur > 0) --cur;
-            for (; pi + cur < n && i + cur < n; ++cur) {
-                if (str[pi + cur] != str[i + cur]) break;
-            }
-            lcp[rsa[i] - 1] = cur;
-        }
+    
+    // build sparse table for calculating lcp
+    void buildSparseTable() {
         st.init(lcp);
     }
-    
-    // calc lcp
-    int getLCP(int a, int b) {          // lcp of str.sutstr(a) and str.substr(b)
-        return st.get(min(rsa[a], rsa[b]), max(rsa[a], rsa[b]));
+
+    // calc lcp of str.sutstr(a) and str.substr(b)
+    int getLCP(int a, int b) {
+        return st.get(min(rank[a], rank[b]), max(rank[a], rank[b]));
     }
 };
 
 
-vector<long long> divisor(long long n) {
-    vector<long long> res;
+//////////////////////////////////////
+// Examples
+//////////////////////////////////////
+
+// ARC 060 F
+void solveARC060F() {
+    string str; 
+    cin >> str;
+    int n = (int)str.size();
+
+    // n の約数を求める
+    vector<long long> divs;
     for (long long i = 1LL; i*i <= n; ++i) {
         if (n%i == 0LL) {
-            res.push_back(i);
+            divs.push_back(i);
             long long temp = n/i;
-            if (i != temp) res.push_back(temp);
+            if (i != temp) divs.push_back(temp);
         }
     }
-    sort(res.begin(), res.end());
-    return res;
-}
+    sort(divs.begin(), divs.end());
 
-
-
-int main() {
-    string str; cin >> str;
-    int n = (int)str.size();
-    vector<long long> divs = divisor(n);
+    // 求める
     long long syuuki = n;
     for (auto d : divs) {
         bool ok = true;
@@ -155,4 +292,49 @@ int main() {
         for (int i = 1; i < n; ++i) if (!cannot_cut[i]) ++con;
         cout << 2 << endl << con << endl;
     }
+}
+
+// AOJ 2644
+void solveAOJ2644() {
+    // Suffix Array の構築
+    string S;
+    cin >> S;
+    SuffixArray<string> suf(S);
+    vector<int> sa = suf.get_sa();
+
+    // Suffix Array の区間に関する RMQ
+    SparseTable<int> min_st(sa);
+    for (auto& v : sa) v = -v;
+    SparseTable<int> max_st(sa);
+
+    auto solve = [&](const string& x, const string& y) -> int {
+        if (!suf.is_contain(x) || !suf.is_contain(y)) {
+            return 0;
+        }
+        int xl = suf.lower_bound(x);
+        int xr = suf.upper_bound(x);
+        int yl = suf.lower_bound(y);
+        int yr = suf.upper_bound(y);
+        int xres = min_st.get(xl, xr);
+        int yres = -max_st.get(yl, yr);
+
+        if (xres > yres || xres + x.size() > yres + y.size()) 
+            return 0;
+        else 
+            return yres - xres + (int)y.size();   
+    };
+
+    // クエリ処理
+    int Q;
+    cin >> Q;
+    while (Q--) {
+        string x, y;
+        cin >> x >> y;
+        cout << solve(x, y) << endl;
+    }
+}
+
+int main() {
+    solveARC060F();
+    //solveAOJ2644();
 }
