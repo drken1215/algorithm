4da216eee8b92edd59d618db5d937e6691344b11
drken1215 <otsuki@monoxer.com>
2023-05-02 14:50:38 +0900

    modify FPS and Bostan-Mori

--- MathAlgebra/formal_power_series.cpp
+++ MathAlgebra/formal_power_series.cpp
@@ -292,40 +292,6 @@ namespace NTT {
     }
 };
 
-// Binomial Coefficient
-template<class T> struct BiCoef {
-    vector<T> fact_, inv_, finv_;
-    constexpr BiCoef() {}
-    constexpr BiCoef(int n) noexcept : fact_(n, 1), inv_(n, 1), finv_(n, 1) {
-        init(n);
-    }
-    constexpr void init(int n) noexcept {
-        fact_.assign(n, 1), inv_.assign(n, 1), finv_.assign(n, 1);
-        int MOD = fact_[0].getmod();
-        for(int i = 2; i < n; i++){
-            fact_[i] = fact_[i-1] * i;
-            inv_[i] = -inv_[MOD%i] * (MOD/i);
-            finv_[i] = finv_[i-1] * inv_[i];
-        }
-    }
-    constexpr T com(int n, int k) const noexcept {
-        if (n < k || n < 0 || k < 0) return 0;
-        return fact_[n] * finv_[k] * finv_[n-k];
-    }
-    constexpr T fact(int n) const noexcept {
-        if (n < 0) return 0;
-        return fact_[n];
-    }
-    constexpr T inv(int n) const noexcept {
-        if (n < 0) return 0;
-        return inv_[n];
-    }
-    constexpr T finv(int n) const noexcept {
-        if (n < 0) return 0;
-        return finv_[n];
-    }
-};
-
 // Formal Power Series
 template <typename mint> struct FPS : vector<mint> {
     using vector<mint>::vector;
