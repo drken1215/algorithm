22fd10d1ca18f2393d4b828ce62fad71f090c42d
drken1215 <otsuki@monoxer.com>
2023-05-23 01:37:22 +0900

    update Miller-Rabin by using MongometryModInt

--- MathNumberTheory/is_prime_Miller_Rabin.cpp
+++ MathNumberTheory/is_prime_Miller_Rabin.cpp
@@ -2,9 +2,9 @@
 // 確率的素数判定 (Miller-Rabin)
 //
 // verifed
-//   アルゴ式 番外編：ミラー-ラビン素数判定法
-//     https://algo-method.com/tasks/513
-//     
+//   Yosupo Library Checker - Primality Test
+//     https://judge.yosupo.jp/problem/primality_test
+//
 
 /*
   {2, 325, 9375, 28178, 450775, 9780504, 1795265022}
@@ -12,40 +12,129 @@
  */
 
 
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 
-// A^N mod M
-template<class T> T pow_mod(T A, T N, T M) {
-    T res = 1 % M;
-    A %= M;
-    while (N) {
-        if (N & 1) res = (res * A) % M;
-        A = (A * A) % M;
-        N >>= 1;
+// montgomery modint (MOD <= 2^64)
+struct MontgomeryModInt64 {
+    using mint = MontgomeryModInt64;
+    using u64 = uint64_t;
+    using u128 = __uint128_t;
+    
+    // static menber
+    static u64 MOD;
+    static u64 INV_MOD;  // INV_MOD * MOD ≡ 1 (mod 2^64)
+    static u64 T128;  // 2^128 (mod MOD)
+    
+    // inner value
+    u64 val;
+    
+    // constructor
+    MontgomeryModInt64() : val(0) { }
+    MontgomeryModInt64(long long v) : val(reduce((u128(v) + MOD) * T128)) { }
+    u64 get() const {
+        u64 res = reduce(val);
+        return res >= MOD ? res - MOD : res;
     }
-    return res;
-}
+    
+    // mod getter and setter
+    static u64 get_mod() { return MOD; }
+    static void set_mod(u64 mod) {
+        assert(mod < (1LL << 62));
+        assert((mod & 1));
+        MOD = mod;
+        T128 = -u128(mod) % mod;
+        INV_MOD = get_inv_mod();
+    }
+    static u64 get_inv_mod() {
+        u64 res = MOD;
+        for (int i = 0; i < 5; ++i) res *= 2 - MOD * res;
+        return res;
+    }
+    static u64 reduce(const u128 &v) {
+        return (v + u128(u64(v) * u64(-INV_MOD)) * MOD) >> 64;
+    }
+    
+    // arithmetic operators
+    mint operator - () const { return mint() - mint(*this); }
+    mint operator + (const mint &r) const { return mint(*this) += r; }
+    mint operator - (const mint &r) const { return mint(*this) -= r; }
+    mint operator * (const mint &r) const { return mint(*this) *= r; }
+    mint operator / (const mint &r) const { return mint(*this) /= r; }
+    mint& operator += (const mint &r) {
+        if ((val += r.val) >= 2 * MOD) val -= 2 * MOD;
+        return *this;
+    }
+    mint& operator -= (const mint &r) {
+        if ((val += 2 * MOD - r.val) >= 2 * MOD) val -= 2 * MOD;
+        return *this;
+    }
+    mint& operator *= (const mint &r) {
+        val = reduce(u128(val) * r.val);
+        return *this;
+    }
+    mint& operator /= (const mint &r) {
+        *this *= r.inv();
+        return *this;
+    }
+    mint inv() const { return pow(MOD - 2); }
+    mint pow(u128 n) const {
+        mint res(1), mul(*this);
+        while (n > 0) {
+            if (n & 1) res *= mul;
+            mul *= mul;
+            n >>= 1;
+        }
+        return res;
+    }
 
-bool is_prime(long long N) {
-    if (N <= 1) return false;
-    if (N == 2 || N == 3) return true;
-    if (N % 2 == 0) return false;
-    vector<long long> A = {2, 325, 9375, 28178, 450775,
-                           9780504, 1795265022};
+    // other operators
+    bool operator == (const mint &r) const {
+        return (val >= MOD ? val - MOD : val) == (r.val >= MOD ? r.val - MOD : r.val);
+    }
+    bool operator != (const mint &r) const {
+        return (val >= MOD ? val - MOD : val) != (r.val >= MOD ? r.val - MOD : r.val);
+    }
+    friend istream& operator >> (istream &is, mint &x) {
+        long long t;
+        is >> t;
+        x = mint(t);
+        return is;
+    }
+    friend ostream& operator << (ostream &os, const mint &x) {
+        return os << x.get();
+    }
+    friend mint modpow(const mint &r, long long n) {
+        return r.pow(n);
+    }
+    friend mint modinv(const mint &r) {
+        return r.inv();
+    }
+};
+
+typename MontgomeryModInt64::u64
+MontgomeryModInt64::MOD, MontgomeryModInt64::INV_MOD, MontgomeryModInt64::T128;
+
+
+// Miller-Rabin
+bool MillerRabin(long long N, vector<long long> A) {
+    using mint = MontgomeryModInt64;
+    mint::set_mod(N);
+    
     long long s = 0, d = N - 1;
     while (d % 2 == 0) {
         ++s;
         d >>= 1;
     }
     for (auto a : A) {
-        if (a % N == 0) return true;
-        long long t, x = pow_mod<__int128_t>(a, d, N);
+        if (N <= a) return true;
+        mint x = mint(a).pow(d);
         if (x != 1) {
+            long long t;
             for (t = 0; t < s; ++t) {
                 if (x == N - 1) break;
-                x = __int128_t(x) * x % N;
+                x *= x;
             }
             if (t == s) return false;
         }
@@ -53,19 +142,38 @@ bool is_prime(long long N) {
     return true;
 }
 
+bool is_prime(long long N) {
+    if (N <= 1) return false;
+    if (N == 2) return true;
+    if (N % 2 == 0) return false;
+    if (N < 4759123141LL)
+        return MillerRabin(N, {2, 7, 61});
+    else
+        return MillerRabin(N, {2, 325, 9375, 28178, 450775, 9780504, 1795265022});
+}
 
-int main() {
-    // 入力
+
+/*/////////////////////////////*/
+// Examples
+/*/////////////////////////////*/
+
+void YosupoPrimarityTest() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
     int N;
     cin >> N;
-    vector<long long> A(N);
-    for (int i = 0; i < N; ++i) cin >> A[i];
-
-    // 素数判定
-    for (auto a : A) {
-        if (is_prime(a))
+    for (int i = 0; i < N; ++i) {
+        long long A;
+        cin >> A;
+        if (is_prime(A))
             cout << "Yes" << endl;
         else
             cout << "No" << endl;
     }
 }
+
+
+int main() {
+    YosupoPrimarityTest();
+}
+
