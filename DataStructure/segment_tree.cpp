88544898abdfea8c85ef000ecc8ab7b919f05013
drken1215 <otsuki@monoxer.com>
2023-06-25 00:29:56 +0900

    modify segtree

--- DataStructure/segment_tree.cpp
+++ DataStructure/segment_tree.cpp
@@ -5,162 +5,169 @@
 //   ARC 008 D - タコヤキオイシクナール
 //     https://beta.atcoder.jp/contests/arc008/tasks/arc008_4
 //
-//   ABC 281 E - Least Elements 
+//   ABC 281 E - Least Elements
 //     https://atcoder.jp/contests/abc281/tasks/abc281_e
+//
+//   ABC 307 F - Virus 2
+//     https://atcoder.jp/contests/abc307/tasks/abc307_f
 //
 
 
-/*
-    セグメントツリーは二項演算の定義されたモノイド上で定義される
-      二項演算関数 f(x, y) を構造体に渡す
-
-
-    // Construction
-    SegTree(n, f, unity): サイズ n に初期化、f は二項演算、unity は単位元 (min なら INF, + なら 0) 
-      ex
-      ・区間和: SegTree<int> seg(n, [](int a, int b){ return a + b; }, 0);
-      ・区間min: SegTree<int> seg(n, [](int a, int b}{ return min(a, b); }, INF);
-
-
-    // Initialization
-    init(n): サイズ n に初期化
-    set(a, v): a 番目の値を v にセットする
-    build(): set した値を元にセグメントツリー全体を構築する、O(n)
-    
-
-    // Queries
-    update(a, v): a 番目の値を v に更新する, O(log n)
-    get(a, b): 区間 [a, b) についての演算結果を返す, O(log n)
-*/
-
-
-#include <iostream>
-#include <vector>
-#include <functional>
-#include <algorithm>
-#include <iomanip>
+#include <bits/stdc++.h>
 using namespace std;
 
 
 // Segment Tree
 template<class Monoid> struct SegTree {
     using Func = function<Monoid(Monoid, Monoid)>;
-    int N;
+
+    // core member
+    int SIZE;
     Func F;
     Monoid IDENTITY;
-    int SIZE_R;
+    
+    // data
+    int offset;
     vector<Monoid> dat;
 
-    /* initialization */
+    // constructor
     SegTree() {}
-    SegTree(int n, const Func f, const Monoid &identity)
-    : N(n), F(f), IDENTITY(identity) {
-        SIZE_R = 1;
-        while (SIZE_R < n) SIZE_R *= 2;
-        dat.assign(SIZE_R * 2, IDENTITY);
+    SegTree(int n, const Func &f, const Monoid &identity)
+    : SIZE(n), F(f), IDENTITY(identity) {
+        offset = 1;
+        while (offset < n) offset *= 2;
+        dat.assign(offset * 2, IDENTITY);
     }
-    void init(int n, const Func f, const Monoid &identity) {  
-        N = n;
+    void init(int n, const Func &f, const Monoid &identity) {
+        SIZE = n;
         F = f;
         IDENTITY = identity;
-        SIZE_R = 1;
-        while (SIZE_R < n) SIZE_R *= 2;
-        dat.assign(SIZE_R * 2, IDENTITY);
+        offset = 1;
+        while (offset < n) offset *= 2;
+        dat.assign(offset * 2, IDENTITY);
     }
+    int size() const { return SIZE; }
     
-    /* set, a is 0-indexed */
-    /* build(): O(N) */
-    void set(int a, const Monoid &v) { dat[a + SIZE_R] = v; }
+    // set, a is 0-indexed //
+    // build(): O(N)
+    void set(int a, const Monoid &v) { dat[a + offset] = v; }
     void build() {
-        for (int k = SIZE_R - 1; k > 0; --k)
+        for (int k = offset - 1; k > 0; --k)
             dat[k] = F(dat[k*2], dat[k*2+1]);
     }
+    void build(const vector<Monoid> &vec) {
+        for (int a = 0; a < vec.size() && a + offset < dat.size(); ++a)
+            set(a, vec[a]);
+        build();
+    }
     
-    /* update a, a is 0-indexed, O(log N) */
+    // update a, a is 0-indexed, O(log N)
     void update(int a, const Monoid &v) {
-        int k = a + SIZE_R;
+        int k = a + offset;
         dat[k] = v;
         while (k >>= 1) dat[k] = F(dat[k*2], dat[k*2+1]);
     }
     
-    /* get [a, b), a and b are 0-indexed, O(log N) */
+    // get [a, b), a and b are 0-indexed, O(log N)
     Monoid get(int a, int b) {
         Monoid vleft = IDENTITY, vright = IDENTITY;
-        for (int left = a + SIZE_R, right = b + SIZE_R; left < right; 
+        for (int left = a + offset, right = b + offset; left < right;
         left >>= 1, right >>= 1) {
             if (left & 1) vleft = F(vleft, dat[left++]);
             if (right & 1) vright = F(dat[--right], vright);
         }
         return F(vleft, vright);
     }
-    Monoid all_get() { return dat[1]; }
-    Monoid operator [] (int a) { return dat[a + SIZE_R]; }
+    Monoid get_all() { return dat[1]; }
+    Monoid operator [] (int a) const { return dat[a + offset]; }
     
-    /* get max r that f(get(l, r)) = True (0-indexed), O(log N) */
-    /* f(IDENTITY) need to be True */
+    // get max r that f(get(l, r)) = True (0-indexed), O(log N)
+    // f(IDENTITY) need to be True
     int max_right(const function<bool(Monoid)> f, int l = 0) {
-        if (l == N) return N;
-        l += SIZE_R;
+        if (l == SIZE) return SIZE;
+        l += offset;
         Monoid sum = IDENTITY;
         do {
             while (l % 2 == 0) l >>= 1;
             if (!f(F(sum, dat[l]))) {
-                while (l < SIZE_R) {
+                while (l < offset) {
                     l = l * 2;
                     if (f(F(sum, dat[l]))) {
                         sum = F(sum, dat[l]);
                         ++l;
                     }
                 }
-                return l - SIZE_R;
+                return l - offset;
             }
             sum = F(sum, dat[l]);
             ++l;
         } while ((l & -l) != l);  // stop if l = 2^e
-        return N;
+        return SIZE;
     }
 
-    /* get min l that f(get(l, r)) = True (0-indexed), O(log N) */
-    /* f(IDENTITY) need to be True */
+    // get min l that f(get(l, r)) = True (0-indexed), O(log N)
+    // f(IDENTITY) need to be True
     int min_left(const function<bool(Monoid)> f, int r = -1) {
         if (r == 0) return 0;
-        if (r == -1) r = N;
-        r += SIZE_R;
+        if (r == -1) r = SIZE;
+        r += offset;
         Monoid sum = IDENTITY;
         do {
             --r;
             while (r > 1 && (r % 2)) r >>= 1;
             if (!f(F(dat[r], sum))) {
-                while (r < SIZE_R) {
+                while (r < offset) {
                     r = r * 2 + 1;
                     if (f(F(dat[r], sum))) {
                         sum = F(dat[r], sum);
                         --r;
                     }
                 }
-                return r + 1 - SIZE_R;
+                return r + 1 - offset;
             }
             sum = F(dat[r], sum);
         } while ((r & -r) != r);
         return 0;
     }
     
-    /* debug */
-    void print() {
-        for (int i = 0; i < N; ++i) {
-            cout << (*this)[i];
-            if (i != N-1) cout << ",";
+    // debug
+    friend ostream& operator << (ostream &s, const SegTree &seg) {
+        for (int i = 0; i < seg.size(); ++i) {
+            s << seg[i];
+            if (i != seg.size()-1) s << " ";
         }
-        cout << endl;
+        return s;
     }
 };
 
-////////////////////////////////////////////
+
+/*/////////////////////////////*/
+// Examples
+/*/////////////////////////////*/
+
+// template chmax, chmin
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+// template debug stream
+#define COUT(x) cout << #x << " = " << (x) << " (L" << __LINE__ << ")" << endl
+template<class T1, class T2> ostream& operator << (ostream &s, pair<T1,T2> P)
+{ return s << '<' << P.first << ", " << P.second << '>'; }
+template<class T> ostream& operator << (ostream &s, vector<T> P)
+{ for (int i = 0; i < P.size(); ++i) { if (i > 0) { s << " "; } s << P[i]; } return s; }
+template<class T> ostream& operator << (ostream &s, deque<T> P)
+{ for (int i = 0; i < P.size(); ++i) { if (i > 0) { s << " "; } s << P[i]; } return s; }
+template<class T> ostream& operator << (ostream &s, vector<vector<T> > P)
+{ for (int i = 0; i < P.size(); ++i) { s << endl << P[i]; } return s << endl; }
+template<class T> ostream& operator << (ostream &s, set<T> P)
+{ for(auto it : P) { s << "<" << it << "> "; } return s; }
+template<class T1, class T2> ostream& operator << (ostream &s, map<T1,T2> P)
+{ for(auto it : P) { s << "<" << it.first << "->" << it.second << "> "; } return s; }
 
 // ARC 008 D - タコヤキオイシクナール
-void solveARC008D() {
+void ARC_008_D() {
     // 入力
-    long long N; 
+    long long N;
     int M;
     cin >> N >> M;
     vector<long long> p(M), pls;
@@ -192,7 +199,8 @@ void solveARC008D() {
     cout << fixed << setprecision(10) << Min << endl << Max << endl;
 }
 
-void solveABC281E() {
+// ABC 281 E
+void ABC_281_E() {
     // 入力
     long long N, M, K;
     cin >> N >> M >> K;
@@ -222,7 +230,7 @@ void solveABC281E() {
         seg.update(id, zero);
     };
     auto get = [&]() -> long long {
-        int r = seg.max_right([&](Monoid r) { 
+        int r = seg.max_right([&](Monoid r) {
             return r.first <= K;
         });
         return seg.get(0, r).second;
@@ -232,14 +240,81 @@ void solveABC281E() {
     for (int i = 0; i < N - M + 1; ++i) {
         cout << get() << " ";
         if (i+M < N) {
-            push(A[i+M], order[i+M]); 
+            push(A[i+M], order[i+M]);
             pop(A[i], order[i]);
         }
     }
     cout << endl;
 }
 
-int main() {
-    //solveARC008D();
-    solveABC281E();
+// ABC 307 F
+void ABC_307_F() {
+    const long long INF = 1LL<<60;
+    
+    // 入力
+    using Edge = pair<int, long long>;
+    using Graph = vector<vector<Edge>>;
+    int N, M, K, D;
+    cin >> N >> M;
+    Graph G(N);
+    for (int i = 0; i < M; ++i) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        --u, --v;
+        G[u].emplace_back(v, w);
+        G[v].emplace_back(u, w);
+    }
+    cin >> K;
+    vector<int> A(K);
+    for (int i = 0; i < K; ++i) {
+        cin >> A[i];
+        --A[i];
+    }
+    cin >> D;
+    vector<long long> X(D);
+    for (int i = 0; i < D; ++i) cin >> X[i];
+    
+    // day >= start かつ X[day] >= x となる最小の day を求める
+    SegTree<long long> seg(D, [&](long long a, long long b){return max(a,b);}, -INF);
+    seg.build(X);
+    auto first_okay_day = [&](long long x, int start) -> int {
+        return seg.max_right([&](long long val) {return val < x;}, start);
+    };
+
+    // Dijkstra
+    using Weight = pair<long long, long long>;
+    using Node = pair<Weight, int>;
+    priority_queue<Node, vector<Node>, greater<Node>> que;
+    vector<Weight> dp(N, Weight(INF,INF));
+    for (int i = 0; i < K; ++i) {
+        que.push(Node({0,0}, A[i]));
+        dp[A[i]] = {0,0};
+    }
+    while (!que.empty()) {
+        auto [cur, v] = que.top();
+        que.pop();
+        
+        if (cur > dp[v] || cur.first >= D) continue;
+        for (auto e : G[v]) {
+            Weight nex(cur.first, cur.second + e.second);
+            if (cur.second + e.second > X[cur.first]) {
+                nex = {first_okay_day(e.second, cur.first+1), e.second};
+            }
+            if (chmin(dp[e.first], nex)) {
+                que.push(Node(dp[e.first], e.first));
+            }
+        }
+    }
+    for (int v = 0; v < N; ++v) {
+        if (dp[v].second > 0) ++dp[v].first;
+        cout << (dp[v].first <= D ? dp[v].first : -1) << endl;
+    }
 }
+    
+
+int main() {
+    //ARC_008_D();
+    ABC_281_E();
+    //ABC_307_F();
+}
+
