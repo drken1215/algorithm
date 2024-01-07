09475066a73177437b86c22191716fcf6476e326
drken1215 <otsuki@monoxer.com>
2024-01-07 12:56:35 +0900

    add order

---
+++ MathNumberTheory/order.cpp
@@ -0,0 +1,326 @@
+//
+// calc order
+//   min: x s.t. a^x \equiv 1 (mod p)
+//
+// verified:
+//   AtCoder ABC 335 G - Discrete Logarithm Problems
+//     https://atcoder.jp/contests/abc335/tasks/abc335_g
+//
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// mod_pow, mod_inv
+template<class T> T mod_pow(T a, T n, T m) {
+    T res = 1;
+    while (n > 0) {
+        if (n % 2 == 1) res = res * a % m;
+        a = a * a % m;
+        n >>= 1;
+    }
+    return res;
+};
+
+template<class T> T mod_inv(T a, T m) {
+    T b = m, u = 1, v = 0;
+    while (b > 0) {
+        T t = a / b;
+        a -= t * b, swap(a, b);
+        u -= t * v, swap(u, v);
+    }
+    u %= m;
+    if (u < 0) u += m;
+    return u;
+};
+
+// montgomery modint (MOD < 2^62, MOD is odd)
+struct MontgomeryModInt64 {
+    using mint = MontgomeryModInt64;
+    using u64 = uint64_t;
+    using u128 = __uint128_t;
+    
+    // static menber
+    static u64 MOD;
+    static u64 INV_MOD;  // INV_MOD * MOD ≡ 1 (mod 2^64)
+    static u64 T128;  // 2^128 (mod MOD)
+    
+    // inner value
+    u64 val;
+    
+    // constructor
+    MontgomeryModInt64() : val(0) { }
+    MontgomeryModInt64(long long v) : val(reduce((u128(v) + MOD) * T128)) { }
+    u64 get() const {
+        u64 res = reduce(val);
+        return res >= MOD ? res - MOD : res;
+    }
+    
+    // mod getter and setter
+    static u64 get_mod() { return MOD; }
+    static void set_mod(u64 mod) {
+        assert(mod < (1LL << 62));
+        assert((mod & 1));
+        MOD = mod;
+        T128 = -u128(mod) % mod;
+        INV_MOD = get_inv_mod();
+    }
+    static u64 get_inv_mod() {
+        u64 res = MOD;
+        for (int i = 0; i < 5; ++i) res *= 2 - MOD * res;
+        return res;
+    }
+    static u64 reduce(const u128 &v) {
+        return (v + u128(u64(v) * u64(-INV_MOD)) * MOD) >> 64;
+    }
+    
+    // arithmetic operators
+    mint operator + () const { return mint(*this); }
+    mint operator - () const { return mint() - mint(*this); }
+    mint operator + (const mint &r) const { return mint(*this) += r; }
+    mint operator - (const mint &r) const { return mint(*this) -= r; }
+    mint operator * (const mint &r) const { return mint(*this) *= r; }
+    mint operator / (const mint &r) const { return mint(*this) /= r; }
+    mint& operator += (const mint &r) {
+        if ((val += r.val) >= 2 * MOD) val -= 2 * MOD;
+        return *this;
+    }
+    mint& operator -= (const mint &r) {
+        if ((val += 2 * MOD - r.val) >= 2 * MOD) val -= 2 * MOD;
+        return *this;
+    }
+    mint& operator *= (const mint &r) {
+        val = reduce(u128(val) * r.val);
+        return *this;
+    }
+    mint& operator /= (const mint &r) {
+        *this *= r.inv();
+        return *this;
+    }
+    mint inv() const { return pow(MOD - 2); }
+    mint pow(u128 n) const {
+        mint res(1), mul(*this);
+        while (n > 0) {
+            if (n & 1) res *= mul;
+            mul *= mul;
+            n >>= 1;
+        }
+        return res;
+    }
+
+    // other operators
+    bool operator == (const mint &r) const {
+        return (val >= MOD ? val - MOD : val) == (r.val >= MOD ? r.val - MOD : r.val);
+    }
+    bool operator != (const mint &r) const {
+        return (val >= MOD ? val - MOD : val) != (r.val >= MOD ? r.val - MOD : r.val);
+    }
+    mint& operator ++ () {
+        ++val;
+        if (val >= MOD) val -= MOD;
+        return *this;
+    }
+    mint& operator -- () {
+        if (val == 0) val += MOD;
+        --val;
+        return *this;
+    }
+    mint operator ++ (int) {
+        mint res = *this;
+        ++*this;
+        return res;
+    }
+    mint operator -- (int) {
+        mint res = *this;
+        --*this;
+        return res;
+    }
+    friend istream& operator >> (istream &is, mint &x) {
+        long long t;
+        is >> t;
+        x = mint(t);
+        return is;
+    }
+    friend ostream& operator << (ostream &os, const mint &x) {
+        return os << x.get();
+    }
+    friend mint pow(const mint &r, long long n) {
+        return r.pow(n);
+    }
+    friend mint inv(const mint &r) {
+        return r.inv();
+    }
+};
+
+typename MontgomeryModInt64::u64
+MontgomeryModInt64::MOD, MontgomeryModInt64::INV_MOD, MontgomeryModInt64::T128;
+
+
+// Miller-Rabin
+bool MillerRabin(long long N, const vector<long long> &A) {
+    assert(N % 2 == 1);
+    assert(N < (1LL<<62));
+    using mint = MontgomeryModInt64;
+    mint::set_mod(N);
+    
+    long long s = 0, d = N - 1;
+    while (d % 2 == 0) {
+        ++s;
+        d >>= 1;
+    }
+    for (auto a : A) {
+        if (N <= a) return true;
+        mint x = mint(a).pow(d);
+        if (x != 1) {
+            long long t;
+            for (t = 0; t < s; ++t) {
+                if (x == N - 1) break;
+                x *= x;
+            }
+            if (t == s) return false;
+        }
+    }
+    return true;
+}
+
+bool is_prime(long long N) {
+    if (N <= 1) return false;
+    else if (N == 2) return true;
+    else if (N % 2 == 0) return false;
+    else if (N < 4759123141LL)
+        return MillerRabin(N, {2, 7, 61});
+    else
+        return MillerRabin(N, {2, 325, 9375, 28178, 450775, 9780504, 1795265022});
+}
+
+
+// Pollard's Rho
+unsigned int xor_shift_rng() {
+    static unsigned int tx = 123456789, ty=362436069, tz=521288629, tw=88675123;
+    unsigned int tt = (tx^(tx<<11));
+    tx = ty, ty = tz, tz = tw;
+    return ( tw=(tw^(tw>>19))^(tt^(tt>>8)) );
+}
+
+long long pollard(long long N) {
+    if (N % 2 == 0) return 2;
+    if (is_prime(N)) return N;
+    
+    assert(N < (1LL<<62));
+    using mint = MontgomeryModInt64;
+    mint::set_mod(N);
+    
+    long long step = 0;
+    while (true) {
+        mint r = xor_shift_rng();  // random r
+        auto f = [&](mint x) -> mint { return x * x + r; };
+        mint x = ++step, y = f(x);
+        while (true) {
+            long long p = gcd((y - x).get(), N);
+            if (p == 0 || p == N) break;
+            if (p != 1) return p;
+            x = f(x);
+            y = f(f(y));
+        }
+    }
+}
+
+vector<long long> pollard_prime_factorize(long long N) {
+    if (N == 1) return {};
+    long long p = pollard(N);
+    if (p == N) return {p};
+    vector<long long> left = pollard_prime_factorize(p);
+    vector<long long> right = pollard_prime_factorize(N / p);
+    if (left.size() > right.size()) swap(left, right);
+    left.insert(left.end(), right.begin(), right.end());
+    sort(left.begin(), left.end());
+    return left;
+}
+
+vector<pair<long long, long long>> prime_factorize(long long N) {
+    vector<pair<long long, long long>> res;
+    const auto &prs = pollard_prime_factorize(N);
+    long long prev = -1, num = 0;
+    for (const auto &pr : prs) {
+        if (pr == prev) ++num;
+        else {
+            if (prev != -1) res.emplace_back(prev, num);
+            prev = pr, num = 1;
+        }
+    }
+    if (prev != -1) res.emplace_back(prev, num);
+    return res;
+}
+
+
+// various methods mod prime P
+struct PrimeProcessor {
+    using mint = MontgomeryModInt64;
+    
+    // input prime
+    long long prime;
+    vector<pair<long long, long long>> pf;  // prime factorization of p-1
+    
+    // constructors
+    PrimeProcessor() {}
+    PrimeProcessor(long long p) : prime(p) {
+        init(p);
+    }
+    
+    // initializer
+    void init(long long p) {
+        assert(is_prime(p));
+        prime = p;
+        if (p % 2 == 1) {
+            assert(p < (1LL<<62));
+            prime = p;
+            pf = prime_factorize(prime - 1);
+            mint::set_mod(prime);
+        }
+    }
+    
+    // min: x s.t. a^x \equiv 1 (mod prime)
+    long long calc_order(long long a) {
+        assert(a != 0);
+        if (prime == 2) return 1;
+        long long res = prime - 1;
+        for (const auto &[p, num] : pf) {
+            while (res % p == 0 && mint(a).pow(res / p) == 1) res /= p;
+        }
+        return res;
+    }
+};
+
+
+
+/*/////////////////////////////*/
+// Examples
+/*/////////////////////////////*/
+
+void ABC_335_G() {
+    long long N, P;
+    cin >> N >> P;
+    vector<long long> A(N);
+    for (int i = 0; i < N; ++i) cin >> A[i];
+    
+    PrimeProcessor pp(P);
+    map<long long, long long> ma;
+    for (auto a : A) {
+        long long order = pp.calc_order(a);
+        ++ma[order];
+    }
+
+    long long res = 0;
+    for (auto [v1, num1] : ma) {
+        for (auto [v2, num2] : ma) {
+            if (v2 % v1 == 0) res += num1 * num2;
+        }
+    }
+    cout << res << endl;
+}
+
+
+int main() {
+    ABC_335_G();
+}
