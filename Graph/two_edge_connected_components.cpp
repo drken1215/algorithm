529a6a1845f2a863729d2c5e7a7fd6d9309d7485
drken1215 <dr.ken.1215@gmail.com>
2024-12-19 02:03:11 +0900

    improve low link

--- Graph/two_edge_connected_components.cpp
+++ Graph/two_edge_connected_components.cpp
@@ -6,6 +6,9 @@
 //     http://hos.ac/slides/20110504_graph.pdf
 //
 // verified:
+//   Yosupo Library Checker - Two-Edge-Connected Components
+//     https://judge.yosupo.jp/problem/two_edge_connected_components
+//
 //   ARC 039 D - 旅行会社高橋君
 //     https://arc039.contest.atcoder.jp/tasks/arc039_d
 //
@@ -35,41 +38,38 @@ template<class T> struct Edge {
     T val;
     Edge() : from(-1), to(-1), val(-1) { }
     Edge(int f, int t, T v = -1) : from(f), to(t), val(v) {}
-    friend ostream& operator << (ostream& s, const Edge& E) {
-        return s << E.from << "->" << E.to;
+    friend ostream& operator << (ostream& s, const Edge& e) {
+        return s << e.from << "->" << e.to;
     }
 };
 
 // graph class
 template<class T> struct Graph {
     vector<vector<Edge<T>>> list;
-    vector<vector<Edge<T>>> reversed_list;
     
-    Graph(int n = 0) : list(n), reversed_list(n) { }
-    void init(int n = 0) {
+    Graph() { }
+    Graph(int n) : list(n) { }
+    Graph(const Graph<T> &G) : list(G.list) { }
+    void init(int n) {
         list.assign(n, vector<Edge<T>>());
-        reversed_list.assign(n, vector<Edge<T>>());
     }
+    Graph &operator = (const Graph &g) {
+        list = g.list;
+        return *this;
+    }
+
     const vector<Edge<T>> &operator [] (int i) const { return list[i]; }
-    const vector<Edge<T>> &get_rev_edges(int i) const { return reversed_list[i]; }
     const size_t size() const { return list.size(); }
     const void clear() {
-        list.clear(), reversed_list.clear();
+        list.clear();
     }
     const void resize(int n) {
-        list.resize(n), reversed_list.resize(n);
+        list.resize(n);
     }
-        
+
     void add_edge(int from, int to, T val = -1) {
         list[from].push_back(Edge(from, to, val));
-        reversed_list[to].push_back(Edge(to, from, val));
-    }
-    
-    void add_bidirected_edge(int from, int to, T val = -1) {
-        list[from].push_back(Edge(from, to, val));
         list[to].push_back(Edge(to, from, val));
-        reversed_list[from].push_back(Edge(from, to, val));
-        reversed_list[to].push_back(Edge(to, from, val));
     }
 
     friend ostream &operator << (ostream &s, const Graph &G) {
@@ -83,102 +83,97 @@ template<class T> struct Graph {
     }
 };
 
-// bridge decomposition
-template<class T> struct BridgeDecomposition {
-    // bridge and articulation points
-    vector<int> aps; // articulation points
-    vector<pair<int,int>> brs; // brideges
-    
-    // decomposition
-    vector<vector<int>> scc; // scc[i] := i'th component
-    vector<int> cmp;          // cmp[v] := which component is v belong
-    Graph<T> newG;               // the tree
-    
-    // intermediate results
-    vector<int> seen, ord, low;
+// low-link
+template<class T> struct LowLink {
+    // results
+    vector<int> ord, low;
+    vector<int> aps;         // articulation points
+    vector<Edge<T>> brs;     // brideges
 
     // constructor
-    BridgeDecomposition() { }
-    BridgeDecomposition(const Graph<T> &graph) {
-        solve(graph);
+    LowLink() { }
+    LowLink(const Graph<T> &G) {
+        solve(G);
     }
-    void init(const Graph<T> &graph) {
-        solve(graph);
+    void init(const Graph<T> &G) {
+        solve(G);
     }
-    
-    // calc low-link
-    void dfs_lowlink(const Graph<T> &G, int v, int p = -1) {
-        static int time = 0;
-        seen[v] = true;
-        ord[v] = low[v] = time++;
-        int num_of_child = 0;
-        bool exist = false; // for articulation point
-        for (auto ch_edge : G[v]) {
-            int ch = ch_edge.to;
-            if (seen[ch]) {
-                if (ch != p) low[v] = min(low[v], ord[ch]); // back edge
-                continue;
+
+    // solver
+    int dfs(const Graph<T> &G, int t, int v, int p) {
+        ord[v] = low[v] = t++;
+        int num_of_children = 0;
+        bool exist_articulation = false, is_multiple_edge = false;
+        for (const auto &e : G[v]) {
+            if (ord[e.to] == -1) {
+                num_of_children++;
+                t = dfs(G, t, e.to, v);
+                low[v] = min(low[v], low[e.to]);  // forward edge of DFS-tree
+                exist_articulation |= (p != -1) && (low[e.to] >= ord[v]);
+                if (ord[v] < low[e.to]) brs.push_back(e);
+            } else if (e.to != p || is_multiple_edge) {
+                low[v] = min(low[v], ord[e.to]);  // back edge
+            } else {
+                is_multiple_edge = true;
             }
-            dfs_lowlink(G, ch, v);
-            low[v] = min(low[v], low[ch]); // forward edge of DFS-tree
-            if (ord[v] < low[ch]) brs.emplace_back(v, ch);
-            if (ord[v] <= low[ch]) exist = true;
-            ++num_of_child;
         }
-        if ((p == -1 && num_of_child > 1) || (p != -1 && exist)) aps.emplace_back(v);
-    }
-    
-    // reconstruct tree
-    vector<int> comp;
-    set<pair<int,int>> newEdges;
-    void MakeTreeDFS(const Graph<T> &G, int v, int curcmp) {
-        seen[v] = true;
-        cmp[v] = curcmp;
-        for (auto e : G[v]) {
-            bool sameComp = true;
-            if (seen[e.to]) sameComp = false;
-            if (binary_search(brs.begin(), brs.end(), make_pair(v, e.to))
-                || binary_search(brs.begin(), brs.end(), make_pair(e.to, v)))
-                sameComp = false;
-            
-            if (!sameComp) {
-                int newcmp = cmp[e.to];
-                if (newcmp == -1) continue;
-                if (newcmp != curcmp) {
-                    if (!newEdges.count(make_pair(curcmp, newcmp))
-                        && !newEdges.count(make_pair(newcmp, curcmp))) {
-                        newEdges.insert(make_pair(curcmp, newcmp));
-                        newG.add_bidirected_edge(curcmp, newcmp, e.val);
-                    }
-                }
-            }
-            else MakeTreeDFS(G, e.to, curcmp);
+        if (exist_articulation || (p == -1 && num_of_children > 1)) {
+            aps.emplace_back(v);
         }
-        comp.push_back(v);
+        return t;
     }
-    
-    // main
+
     void solve(const Graph<T> &G) {
-        // calc low-link
-        int N = (int)G.size();
-        seen.assign(N, 0), ord.resize(N), low.resize(N);
-        aps.clear(), brs.clear();
-        for (int v = 0; v < N; ++v) if (!seen[v]) dfs_lowlink(G, v);
-        sort(brs.begin(), brs.end());
-        
-        // reconstruct tree
-        scc.clear();
-        newG.clear(), newG.resize(N);
-        newEdges.clear();
-        seen.assign(N, 0), cmp.assign(N, -1);
-        int tcmp = 0;
-        for (int v = 0; v < N; ++v) {
-            if (seen[v]) continue;
-            comp.clear();
-            MakeTreeDFS(G, v, tcmp++);
-            scc.push_back(comp);
+        ord.assign(G.size(), -1), low.assign(G.size(), -1);
+        for (int v = 0, k = 0; v < (int)G.size(); v++) {
+            if (ord[v] == -1) k = dfs(G, k, v, -1);
         }
-        newG.resize(tcmp);
+    }
+};
+
+// Two-Edge-Connected Components decomposition
+template<class T> struct TwoEdgeConnectedComponentsDecomposition {
+    // results
+    LowLink<T> ll;
+    vector<int> cmp;
+    vector<vector<int>> groups, tree;
+
+    // constructor
+    TwoEdgeConnectedComponentsDecomposition() { }
+    TwoEdgeConnectedComponentsDecomposition(const Graph<T> &G) {
+        solve(G);
+    }
+    void init(const Graph<T> &G) {
+        solve(G);
+    }
+
+    // solver
+    int dfs(const Graph<T> &G, int t, int v, int p) {
+        if (p >= 0 && ll.ord[p] >= ll.low[v]) cmp[v] = cmp[p];
+        else cmp[v] = t++;
+        for (const auto &e : G[v]) {
+            if (cmp[e.to] == -1) t = dfs(G, t, e.to, v);
+        }
+        return t;
+    }
+
+    void solve(const Graph<T> &G) {
+        ll.init(G);
+        cmp.assign(G.size(), -1);
+        int t = 0;
+        for (int v = 0; v < (int)G.size(); v++) {
+            if (cmp[v] == -1) t = dfs(G, t, v, -1);
+        }
+        groups.resize(t);
+        tree.resize(t);
+        for (int v = 0; v < (int)G.size(); v++) {
+            groups[cmp[v]].push_back(v);
+        }
+        for (const auto &e : ll.brs) {
+            int u = cmp[e.from], v = cmp[e.to];
+            tree[u].push_back(v);
+            tree[v].push_back(u);
+        }
     }
 };
 
@@ -188,6 +183,27 @@ template<class T> struct BridgeDecomposition {
 // Examples
 //------------------------------//
 
+// Yosupo Library Checker - Two-Edge-Connected Components
+void YosupoLibraryCheckerTwoEdgeConnectedComponents() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    int N, M, a, b;
+    cin >> N >> M;
+    Graph<int> G(N);
+    for (int i = 0; i < M; i++) {
+        cin >> a >> b;
+        G.add_edge(a, b, i);
+    }
+    TwoEdgeConnectedComponentsDecomposition<int> tecc(G);
+
+    cout << tecc.groups.size() << '\n';
+    for (const auto &group : tecc.groups) {
+        cout << group.size();
+        for (auto v : group) cout << " " << v;
+        cout << '\n';
+    }
+}
+
 // ARC 039 D - 旅行会社高橋君
 void ARC_039_D() {
     int V, E, s, t;
@@ -195,12 +211,12 @@ void ARC_039_D() {
     Graph<int> original_graph(V);
     for (int i = 0; i < E; ++i) {
         cin >> s >> t, s--, t--;
-        original_graph.add_bidirected_edge(s, t, 1);
+        original_graph.add_edge(s, t, 1);
     }
-    BridgeDecomposition bd(original_graph);
+    TwoEdgeConnectedComponentsDecomposition<int> tecc(original_graph);
 
     // LCA
-    auto G = bd.newG;
+    auto tree = tecc.tree;
     int h = 1;
     while ((1<<h) < V) ++h;
     vector<vector<int>> parent(h, vector<int>(V, -1));
@@ -209,7 +225,7 @@ void ARC_039_D() {
     auto dfs = [&](auto dfs, int v, int p, int d) -> void {
         parent[0][v] = p;
         depth[v] = d;
-        for (auto e : G[v]) if (e.to != p) dfs(dfs, e.to, v, d+1);
+        for (auto to : tree[v]) if (to != p) dfs(dfs, to, v, d+1);
     };
 
     dfs(dfs, 0, -1, 0);
@@ -243,7 +259,7 @@ void ARC_039_D() {
     cin >> Q;
     for (int _ = 0; _ < Q; ++_) {
         cin >> a >> b >> c, a--, b--, c--;
-        a = bd.cmp[a], b = bd.cmp[b], c = bd.cmp[c];
+        a = tecc.cmp[a], b = tecc.cmp[b], c = tecc.cmp[c];
         int ab = dist(a, b);
         int bc = dist(b, c);
         int ac = dist(a, c);
@@ -252,7 +268,79 @@ void ARC_039_D() {
     }
 }
 
+// TTPC 2024 DIV1 A - Don't Detect Cycle
+void TTPC_2024_DIV1_A() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N, M, u, v;
+        cin >> N >> M;
+        Graph<int> G(N);
+        for (int i = 0; i < M; i++) {
+            cin >> u >> v, u--, v--;
+            G.add_edge(u, v, i);
+        }
+
+        vector<int> res, former, latter;
+        while (true) {
+            set<pair<int,int>> ers;
+
+            // erase bridges
+            LowLink<int> ll(G);
+            for (auto e : ll.brs) {
+                former.push_back(e.val);
+                ers.insert(minmax(e.from, e.to));
+            }
+
+            // erase edges whose endpoints with 2 degree
+            vector<int> deg(N, 0);
+            for (int v = 0; v < N; v++) {
+                for (auto e : G[v]) {
+                    if (ers.count(minmax(e.from, e.to))) continue;
+                    deg[v]++;
+                }
+            }
+            for (int v = 0; v < N; v++) {
+                for (auto e : G[v]) {
+                    if (e.from > e.to) continue;
+                    if (ers.count(minmax(e.from, e.to))) continue;
+                    if (deg[e.from] == 2 && deg[e.to] == 2) {
+                        latter.push_back(e.val);
+                        ers.insert(minmax(e.from, e.to));
+                    }
+                }
+            }
+
+            if (ers.empty()) break;
+
+            // build new graph
+            Graph<int> G2(N);
+            for (int v = 0; v < N; v++) {
+                for (auto e : G[v]) {
+                    if (e.from > e.to) continue;
+                    if (ers.count(minmax(e.from, e.to))) continue;
+                    G2.add_edge(e.from, e.to, e.val);
+                }
+            }
+            G = G2;
+        }
+
+        reverse(latter.begin(), latter.end());
+        for (auto v : former) res.push_back(v);
+        for (auto v : latter) res.push_back(v);
+        if (res.size() < M) {
+            cout << -1 << endl;
+        } else {
+            for (auto id : res) cout << id+1 << " ";
+            cout << endl;
+        }
+    }
+}
+
 
 int main() {
-    ARC_039_D();
+    //YosupoLibraryCheckerTwoEdgeConnectedComponents();
+    //ARC_039_D();
+    TTPC_2024_DIV1_A();
 }
