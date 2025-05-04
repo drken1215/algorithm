6cf9da183a2c3c519994b8ebc542c5461d6d44a3
drken1215 <dr.ken.1215@gmail.com>
2025-05-04 15:21:14 +0900

    improve i128

--- MathNumberTheory/int128.cpp
+++ MathNumberTheory/int128.cpp
@@ -12,182 +12,40 @@ using namespace std;
 
 
 // int 128
-struct i128 {
-    // inner value
-    __int128 val;
-    
-    // constructor
-    constexpr i128() : val(0) {}
-    constexpr i128(long long v) : val(v) {}
-    i128(const string &s) : val(0) {
-        parse(s);
+using i128 = __int128;
+constexpr i128 to_integer(const string &s) {
+    i128 res = 0;
+    for (auto c : s) {
+         if (isdigit(c)) res = res * 10 + (c - '0');
     }
-    void parse(const string &s) {
-        val = 0;
-        for (auto c : s) {
-            if (isdigit(c)) val = val * 10 + (c - '0');
-        }
-        if (s[0] == '-') val *= -1;
+    if (s[0] == '-') res *= -1;
+    return res;
+}
+istream& operator >> (istream &is, i128 &x) {
+    string s;
+    is >> s;
+    x = to_integer(s);
+    return is;
+}
+ostream& operator << (ostream &os, const i128 &x) {
+    i128 ax = (x >= 0 ? x : -x);
+    char buffer[128];
+    char *d = end(buffer);
+    do {
+         --d;
+        *d = "0123456789"[ax % 10];
+        ax /= 10;
+    } while (ax != 0);
+    if (x < 0) {
+        --d;
+        *d = '-';
     }
-    constexpr __int128 get() const {
-        return val;
+    int len = end(buffer) - d;
+    if (os.rdbuf()->sputn(d, len) != len) {
+        os.setstate(ios_base::badbit);
     }
-    constexpr i128 abs() {
-        if (val < 0) return -val;
-        else return val;
-    }
-    
-    // comparison operators
-    constexpr bool operator == (const i128 &r) const {
-        return this->val == r.val;
-    }
-    constexpr bool operator != (const i128 &r) const {
-        return this->val != r.val;
-    }
-    constexpr bool operator < (const i128 &r) const {
-        return this->val < r.val;
-    }
-    constexpr bool operator > (const i128 &r) const {
-        return this->val > r.val;
-    }
-    constexpr bool operator <= (const i128 &r) const {
-        return this->val <= r.val;
-    }
-    constexpr bool operator >= (const i128 &r) const {
-        return this->val >= r.val;
-    }
-    friend constexpr bool operator < (long long x, const i128 &y) {
-        return y.val > x;
-    }
-    friend constexpr bool operator > (long long x, const i128 &y) {
-        return y.val < x;
-    }
-    friend constexpr bool operator <= (long long x, const i128 &y) {
-        return y.val >= x;
-    }
-    friend constexpr bool operator >= (long long x, const i128 &y) {
-        return y.val <= x;
-    }
-    
-    // arithmetic operators
-    constexpr i128& operator += (const i128 &r) {
-        val += r.val;
-        return *this;
-    }
-    constexpr i128& operator -= (const i128 &r) {
-        val -= r.val;
-        return *this;
-    }
-    constexpr i128& operator *= (const i128 &r) {
-        val *= r.val;
-        return *this;
-    }
-    constexpr i128& operator /= (const i128 &r) {
-        val /= r.val;
-        return *this;
-    }
-    constexpr i128& operator %= (const i128 &r) {
-        val %= r.val;
-        return *this;
-    }
-    constexpr i128 operator + () const {
-        return i128(*this);
-    }
-    constexpr i128 operator - () const {
-        return i128(0) - i128(*this);
-    }
-    constexpr i128 operator + (const i128 &r) const {
-        return i128(*this) += r;
-    }
-    constexpr i128 operator - (const i128 &r) const {
-        return i128(*this) -= r;
-    }
-    constexpr i128 operator * (const i128 &r) const {
-        return i128(*this) *= r;
-    }
-    constexpr i128 operator / (const i128 &r) const {
-        return i128(*this) /= r;
-    }
-    constexpr i128 operator % (const i128 &r) const {
-        return i128(*this) %= r;
-    }
-    
-    // bit operators
-    constexpr i128 operator >>= (long long r) {
-        val >>= r;
-        return *this;
-    }
-    constexpr i128 operator <<= (long long r) {
-        val <<= r;
-        return *this;
-    }
-    constexpr i128 operator &= (long long r) {
-        val &= r;
-        return *this;
-    }
-    constexpr i128 operator |= (long long r) {
-        val |= r;
-        return *this;
-    }
-    constexpr i128 operator << (long long r) const {
-        return i128(*this) <<= r;
-    }
-    constexpr i128 operator >> (long long r) const {
-        return i128(*this) >>= r;
-    }
-    constexpr i128 operator & (long long r) const {
-        return i128(*this) &= r;
-    }
-    constexpr i128 operator | (long long r) const {
-        return i128(*this) |= r;
-    }
-    
-    // other operators
-    constexpr i128& operator ++ () {
-        ++val;
-        return *this;
-    }
-    constexpr i128& operator -- () {
-        --val;
-        return *this;
-    }
-    constexpr i128 operator ++ (int) {
-        i128 res = *this;
-        ++*this;
-        return res;
-    }
-    constexpr i128 operator -- (int) {
-        i128 res = *this;
-        --*this;
-        return res;
-    }
-    friend istream& operator >> (istream &is, i128 &x) {
-        string s;
-        is >> s;
-        x.parse(s);
-        return is;
-    }
-    friend ostream& operator << (ostream &os, const i128 &x) {
-        auto tmp = x.val < 0 ? -x.val : x.val;
-        char buffer[128];
-        char *d = end(buffer);
-        do {
-            --d;
-            *d = "0123456789"[tmp % 10];
-            tmp /= 10;
-        } while (tmp != 0);
-        if (x.val < 0) {
-            --d;
-            *d = '-';
-        }
-        int len = end(buffer) - d;
-        if (os.rdbuf()->sputn(d, len) != len) {
-            os.setstate(ios_base::badbit);
-        }
-        return os;
-    }
-};
-
+    return os;
+}
 
 
 //------------------------------//
@@ -196,7 +54,7 @@ struct i128 {
 
 void mini_test() {
     i128 a = 99;
-    i128 b("993421434234324234234432421234324");
+    i128 b = to_integer("993421434234324234234432421234324");
     
     cout << a + b << endl;
     cout << a * b << endl;
