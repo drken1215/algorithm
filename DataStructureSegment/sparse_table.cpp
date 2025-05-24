3667089b3e0addaef81dacc63f37cc29197e7061
drken1215 <dr.ken.1215@gmail.com>
2025-05-25 02:42:04 +0900

    add

--- DataStructureSegment/sparse_table.cpp
+++ DataStructureSegment/sparse_table.cpp
@@ -1,9 +1,9 @@
 //
 // RMQ (by sparse table)
 //
-// verified (suffix array の lcp を sparse table で求める):
-//   ARC 060 F - 最良表現
-//     https://beta.atcoder.jp/contests/arc060/tasks/arc060_d
+// verified:
+//   ABC 407 F - Sums of Sliding Window Maximum
+//     https://atcoder.jp/contests/abc407/tasks/abc407_f
 //
 
 
@@ -17,21 +17,23 @@
 */
 
 
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
 
 // Sparse Table
 template<class MeetSemiLattice> struct SparseTable {
+    using Func = function<MeetSemiLattice(MeetSemiLattice, MeetSemiLattice)>;
+
+    // core member
+    Func OP;
     vector<vector<MeetSemiLattice>> dat;
     vector<int> height;
     
     SparseTable() { }
-    SparseTable(const vector<MeetSemiLattice> &vec) { init(vec); }
-    void init(const vector<MeetSemiLattice> &vec) {
+    SparseTable(const vector<MeetSemiLattice> &vec, const Func &op)  { init(vec, op); }
+    void init(const vector<MeetSemiLattice> &vec, const Func &op) {
+        OP = op;
         int n = (int)vec.size(), h = 1;
         while ((1<<h) <= n) ++h;
         dat.assign(h, vector<MeetSemiLattice>(1<<h));
@@ -40,133 +42,62 @@ template<class MeetSemiLattice> struct SparseTable {
         for (int i = 0; i < n; ++i) dat[0][i] = vec[i];
         for (int i = 1; i < h; ++i) {
             for (int j = 0; j < n; ++j)
-                dat[i][j] = min(dat[i-1][j], dat[i-1][min(j+(1<<(i-1)),n-1)]);
+                dat[i][j] = OP(dat[i-1][j], dat[i-1][min(j+(1<<(i-1)),n-1)]);
         }
     }
     
     MeetSemiLattice get(int a, int b) {
-        return min(dat[height[b-a]][a], dat[height[b-a]][b-(1<<height[b-a])]);
+        return OP(dat[height[b-a]][a], dat[height[b-a]][b-(1<<height[b-a])]);
     }
 };
-
-
-// Suffix Array ( Manber&Myers: O(n (logn)^2) )
-struct SuffixArray {
-    string str;
-    vector<int> sa;         // sa[i] : the starting index of the i-th smallest suffix (i = 0, 1, ..., n)
-    vector<int> lcp;        // lcp[i]: the lcp of sa[i] and sa[i+1] (i = 0, 1, ..., n-1)
-    inline int& operator [] (int i) {return sa[i];}
-    
-    SuffixArray(const string& str_) : str(str_) { buildSA(); calcLCP(); }
-    void init(const string& str_) { str = str_; buildSA(); calcLCP(); }
-    
-    // build SA
-    vector<int> rank_sa, tmp_rank_sa;
-    struct CompareSA {
-        int n, k;
-        const vector<int> &rank;
-        CompareSA(int n, int k, const vector<int> &rank_sa) : n(n), k(k), rank(rank_sa) {}
-        bool operator()(int i, int j) {
-            if (rank[i] != rank[j]) return (rank[i] < rank[j]);
-            else {
-                int rank_ik = (i + k <= n ? rank[i + k] : -1);
-                int rank_jk = (j + k <= n ? rank[j + k] : -1);
-                return (rank_ik < rank_jk);
-            }
-        }
-    };
-    void buildSA() {
-        int n = (int)str.size();
-        sa.resize(n+1), lcp.resize(n+1), rank_sa.resize(n+1), tmp_rank_sa.resize(n+1);
-        for (int i = 0; i < n; ++i) sa[i] = i, rank_sa[i] = (int)str[i];
-        sa[n] = n, rank_sa[n] = -1;
-        for (int k = 1; k <= n; k *= 2) {
-            CompareSA csa(n, k, rank_sa);
-            sort(sa.begin(), sa.end(), csa);
-            tmp_rank_sa[sa[0]] = 0;
-            for (int i = 1; i <= n; ++i) {
-                tmp_rank_sa[sa[i]] = tmp_rank_sa[sa[i - 1]];
-                if (csa(sa[i - 1], sa[i])) ++tmp_rank_sa[sa[i]];
-            }
-            for (int i = 0; i <= n; ++i) rank_sa[i] = tmp_rank_sa[i];
-        }
-    }
-    vector<int> rsa;
-    SparseTable<int> st;
-    void calcLCP() {
-        int n = (int)str.size();
-        rsa.resize(n+1);
-        for (int i = 0; i <= n; ++i) rsa[sa[i]] = i;
-        lcp.resize(n+1);
-        lcp[0] = 0;
-        int cur = 0;
-        for (int i = 0; i < n; ++i) {
-            int pi = sa[rsa[i] - 1];
-            if (cur > 0) --cur;
-            for (; pi + cur < n && i + cur < n; ++cur) {
-                if (str[pi + cur] != str[i + cur]) break;
-            }
-            lcp[rsa[i] - 1] = cur;
-        }
-        st.init(lcp);
-    }
-    
-    // calc lcp
-    int getLCP(int a, int b) {          // lcp of str.sutstr(a) and str.substr(b)
-        return st.get(min(rsa[a], rsa[b]), max(rsa[a], rsa[b]));
-    }
-};
-
 
 
 //------------------------------//
 // Examples
 //------------------------------//
 
-vector<long long> divisor(long long n) {
-    vector<long long> res;
-    for (long long i = 1LL; i*i <= n; ++i) {
-        if (n%i == 0LL) {
-            res.push_back(i);
-            long long temp = n/i;
-            if (i != temp) res.push_back(temp);
-        }
+// ABC 407 F - Sums of Sliding Window Maximum
+void ABC_407_F() {
+    int N;
+    cin >> N;
+    using Node = pair<long long, int>;
+    vector<Node> A(N);
+    for (int i = 0; i < N; i++) {
+        cin >> A[i].first;
+        A[i].second = i;
     }
-    sort(res.begin(), res.end());
-    return res;
+    SparseTable<Node> st(A, [&](Node a, Node b){return max(a, b);});
+
+    // imos method
+    vector<long long> res(N+3, 0);
+    auto imos = [&](int l, int r, long long fval, long long d) -> void {
+        // 区間 [l, r) に、初項 fval, 公差 d の等差数列を足す
+        long long lval = fval + d * (r - l - 1);
+        res[l] += fval, res[l+1] += -fval + d, res[r] += -lval - d, res[r+1] += lval;
+    };
+
+    // 再帰的に最大値を足し込む
+    auto rec = [&](auto &&rec, int l, int r) -> void {
+        if (r - l <= 0) return;
+        auto [val, m] = st.get(l, r);
+        int L = min(m+1-l, r-m), R = max(m+1-l, r-m);
+        imos(1, L+1, val, val);
+        imos(L+1, R+1, val * L, 0);
+        imos(R+1, r-l+1, val * (L-1), -val);
+        rec(rec, l, m);
+        rec(rec, m+1, r);
+    };
+    rec(rec, 0, N);
+
+    // imos の復元 (2 回累積和をとる)
+    for (int i = 0; i+1 < res.size(); i++) res[i+1] += res[i];
+    for (int i = 0; i+1 < res.size(); i++) res[i+1] += res[i];
+
+    // 出力
+    for (int k = 1; k <= N; k++) cout << res[k] << '\n';
 }
 
+
 int main() {
-    string str; cin >> str;
-    int n = (int)str.size();
-    vector<long long> divs = divisor(n);
-    long long syuuki = n;
-    for (auto d : divs) {
-        bool ok = true;
-        for (int j = 0; j + d < n; ++j) {
-            if (str[j] != str[j+d]) ok = false;
-        }
-        if (ok) syuuki = min(syuuki, d);
-    }
-    if (syuuki == n) cout << 1 << endl << 1 << endl;
-    else if (syuuki == 1) cout << n << endl << 1 << endl;
-    else {
-        vector<int> cannot_cut(n*2, 0);
-        SuffixArray sa(str);
-        for (int d = 1; d < n; ++d) {
-            for (int dd = d; dd < n; dd += d) {
-                int lcp = sa.getLCP(0, dd);
-                if (lcp < d) break;
-                cannot_cut[dd + d] = true;
-            }
-            for (int dd = n-d*2; dd >= 0; dd -= d) {
-                int lcp = sa.getLCP(dd, n-d);
-                if (lcp < d) break;
-                cannot_cut[dd] = true;
-            }
-        }
-        int con = 0;
-        for (int i = 1; i < n; ++i) if (!cannot_cut[i]) ++con;
-        cout << 2 << endl << con << endl;
-    }
+    ABC_407_F();
 }
