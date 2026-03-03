766eb1d330ea02c30c170f143c41edfd4005070d
drken1215 <dr.ken.1215@gmail.com>
2026-03-03 22:15:43 +0900

    add

--- template_atcoder.cpp
+++ template_atcoder.cpp
@@ -959,6 +959,44 @@ template<int MOD = 998244353, bool PRIME = true> struct Fp {
     }
 };
 
+// Binomial coefficient
+template<class mint> struct BiCoef {
+    vector<mint> fact_, inv_, finv_;
+    constexpr BiCoef() {}
+    constexpr BiCoef(int n) : fact_(n, 1), inv_(n, 1), finv_(n, 1) {
+        init(n);
+    }
+    constexpr void init(int n) {
+        fact_.assign(n, 1), inv_.assign(n, 1), finv_.assign(n, 1);
+        int MOD = fact_[0].get_mod();
+        for(int i = 2; i < n; i++){
+            fact_[i] = fact_[i-1] * i;
+            inv_[i] = -inv_[MOD%i] * (MOD/i);
+            finv_[i] = finv_[i-1] * inv_[i];
+        }
+    }
+    constexpr mint com(int n, int k) const {
+        if (n < k || n < 0 || k < 0) return 0;
+        return fact_[n] * finv_[k] * finv_[n-k];
+    }
+    constexpr mint fact(int n) const {
+        if (n < 0) return 0;
+        return fact_[n];
+    }
+    constexpr mint inv(int n) const {
+        if (n < 0) return 0;
+        return inv_[n];
+    }
+    constexpr mint finv(int n) const {
+        if (n < 0) return 0;
+        return finv_[n];
+    }
+    // 1 / (1 - x)^n の r 次の係数
+    constexpr mint negcom(int n, int r) const {
+        return com(n + r - 1, r);
+    }
+};
+
 // dynamic modint
 struct DynamicModint {
     using mint = DynamicModint;
@@ -1083,44 +1121,6 @@ struct DynamicModint {
     }
 };
 int DynamicModint::MOD;
-
-// Binomial coefficient
-template<class mint> struct BiCoef {
-    vector<mint> fact_, inv_, finv_;
-    constexpr BiCoef() {}
-    constexpr BiCoef(int n) : fact_(n, 1), inv_(n, 1), finv_(n, 1) {
-        init(n);
-    }
-    constexpr void init(int n) {
-        fact_.assign(n, 1), inv_.assign(n, 1), finv_.assign(n, 1);
-        int MOD = fact_[0].get_mod();
-        for(int i = 2; i < n; i++){
-            fact_[i] = fact_[i-1] * i;
-            inv_[i] = -inv_[MOD%i] * (MOD/i);
-            finv_[i] = finv_[i-1] * inv_[i];
-        }
-    }
-    constexpr mint com(int n, int k) const {
-        if (n < k || n < 0 || k < 0) return 0;
-        return fact_[n] * finv_[k] * finv_[n-k];
-    }
-    constexpr mint fact(int n) const {
-        if (n < 0) return 0;
-        return fact_[n];
-    }
-    constexpr mint inv(int n) const {
-        if (n < 0) return 0;
-        return inv_[n];
-    }
-    constexpr mint finv(int n) const {
-        if (n < 0) return 0;
-        return finv_[n];
-    }
-    // 1 / (1 - x)^n の r 次の係数
-    constexpr mint negcom(int n, int r) const {
-        return com(n + r - 1, r);
-    }
-};
 
 // all inverse
 template<class mint> vector<mint> all_inverse(const vector<mint> &v) {
