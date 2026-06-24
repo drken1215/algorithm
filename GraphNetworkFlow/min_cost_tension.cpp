5238bcb55e155d26e048edb52cd40b2e6cc0d509
otsuki <otsuki@ITA-547bc9f1.local>
2026-06-24 15:12:39 +0900

    add example for min-cost tension

--- GraphNetworkFlow/min_cost_tension.cpp
+++ GraphNetworkFlow/min_cost_tension.cpp
@@ -15,6 +15,25 @@
 //   AtCoder ABC 397 G - Maximize Distance
 //     https://atcoder.jp/contests/abc397/tasks/abc397_g
 //
+//   HUPC 2020 H - Traditional Company (AOJ 3171)
+//     https://onlinejudge.u-aizu.ac.jp/problems/3171
+//
+
+/*
+    min_{p}: 
+        Σ_{v} b(v)p(v) + Σ_{e} {c(e) max(0, p(v) - p(u) - l(e)}
+    s.t.
+        p(v) - p(u) <= d(e)
+    ->
+        b-flow (with demand-suply: b)
+        edge 
+            obj func: e = (u, v) with capacity c(e), cost l(e)
+            constraint: e = (u, v) with capacity INF, cost d(e)
+        optimal value *= -1
+
+    in general:
+        Σ_{v} b(v)p(v) + Σ_{e} f(p(v) - p(u)), where f is concave
+*/
 
 
 #pragma GCC optimize("Ofast")
@@ -22,88 +41,6 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-
-
-using ll = long long;
-using i128 = __int128_t;
-using u128 = __uint128_t;
-using pint = pair<int, int>;
-using pll = pair<long long, long long>;
-using tll = array<long long, 3>;
-using fll = array<long long, 4>;
-using vint = vector<int>;
-using vll = vector<long long>;
-using dint = deque<int>;
-using dll = deque<long long>;
-using vvint = vector<vector<int>>;
-using vvll = vector<vector<long long>>;
-using vpll = vector<pair<long long, long long>>;
-template<class T> using min_priority_queue = priority_queue<T, vector<T>, greater<T>>;
-
-template<class S, class T> inline bool chmax(S &a, T b) { return (a < b ? a = b, 1 : 0); }
-template<class S, class T> inline bool chmin(S &a, T b) { return (a > b ? a = b, 1 : 0); }
-template<class S, class T> inline auto maxll(S a, T b) { return max(ll(a), ll(b)); }
-template<class S, class T> inline auto minll(S a, T b) { return min(ll(a), ll(b)); }
-template<class T> auto max(const T &a) { return *max_element(a.begin(), a.end()); }
-template<class T> auto min(const T &a) { return *min_element(a.begin(), a.end()); }
-template<class T> auto argmax(const T &a) { return max_element(a.begin(), a.end()) - a.begin(); }
-template<class T> auto argmin(const T &a) { return min_element(a.begin(), a.end()) - a.begin(); }
-template<class T> auto accum(const vector<T> &a) { return accumulate(a.begin(), a.end(), T()); }
-template<class T> auto accum(const deque<T> &a) { return accumulate(a.begin(), a.end(), T()); }
-
-#define REP(i, a) for (long long i = 0; i < (long long)(a); i++)
-#define REP2(i, a, b) for (long long i = a; i < (long long)(b); i++)
-#define RREP(i, a) for (long long i = (a)-1; i >= (long long)(0); --i)
-#define RREP2(i, a, b) for (long long i = (b)-1; i >= (long long)(a); --i)
-#define EB emplace_back
-#define PF push_front
-#define PB push_back
-#define MP make_pair
-#define FI first
-#define SE second
-#define ALL(x) x.begin(), x.end()
-#define COUT(x) cout << #x << " = " << (x) << " (L" << __LINE__ << ")" << endl
-
-// input
-template<class T> istream& operator >> (istream &is, vector<T> &P)
-{ for (int i = 0; i < (int)P.size(); ++i) cin >> P[i]; return is; }
-template<class T> istream& operator >> (istream &is, deque<T> &P)
-{ for (int i = 0; i < (int)P.size(); ++i) cin >> P[i]; return is; }
-template<class T> istream& operator >> (istream &is, vector<vector<T>> &P)
-{ for (int i = 0; i < (int)P.size(); ++i) cin >> P[i]; return is; }
-
-// output
-template<class S, class T> ostream& operator << (ostream &s, const pair<S, T> &P)
-{ return s << '<' << P.first << ", " << P.second << '>'; }
-template<class T> ostream& operator << (ostream &s, const array<T, 2> &P)
-{ return s << '<' << P[0] << "," << P[1] << '>'; }
-template<class T> ostream& operator << (ostream &s, const array<T, 3> &P)
-{ return s << '<' << P[0] << "," << P[1] << "," << P[2] << '>'; }
-template<class T> ostream& operator << (ostream &s, const array<T, 4> &P)
-{ return s << '<' << P[0] << "," << P[1] << "," << P[2] << "," << P[3] << '>'; }
-template<class T> ostream& operator << (ostream &s, const vector<T> &P)
-{ for (int i = 0; i < P.size(); ++i) { if (i > 0) { s << " "; } s << P[i]; } return s; }
-template<class T> ostream& operator << (ostream &s, const deque<T> &P)
-{ for (int i = 0; i < P.size(); ++i) { if (i > 0) { s << " "; } s << P[i]; } return s; }
-template<class T> ostream& operator << (ostream &s, const vector<vector<T>> &P)
-{ for (int i = 0; i < P.size(); ++i) { s << endl << P[i]; } return s << endl; }
-template<class T> ostream& operator << (ostream &s, const set<T> &P)
-{ for (auto it : P) { s << "<" << it << "> "; } return s; }
-template<class T> ostream& operator << (ostream &s, const multiset<T> &P)
-{ for (auto it : P) { s << "<" << it << "> "; } return s; }
-template<class T> ostream& operator << (ostream &s, const unordered_set<T> &P)
-{ for (auto it : P) { s << "<" << it << "> "; } return s; }
-template<class S, class T> ostream& operator << (ostream &s, const map<S, T> &P)
-{ for (auto it : P) { s << "<" << it.first << "->" << it.second << "> "; } return s; }
-template<class S, class T> ostream& operator << (ostream &s, const unordered_map<S, T> &P)
-{ for (auto it : P) { s << "<" << it.first << "->" << it.second << "> "; } return s; }
-void yes(bool a) { cout << (a ? "yes" : "no") << endl; }
-void YES(bool a) { cout << (a ? "YES" : "NO") << endl; }
-void Yes(bool a) { cout << (a ? "Yes" : "No") << endl; }
-const vector<int> DX = {1, 0, -1, 0, 1, -1, 1, -1};
-const vector<int> DY = {0, 1, 0, -1, 1, -1, -1, 1};
-
-
 
 
 //--------------------------------//
@@ -862,9 +799,55 @@ void ABC_397_G() {
     cout << low << endl;
 }
 
+// HUPC 2020 H - Traditional Company (AOJ 3171)
+/*
+    T 以内にできるか？　以下を -X 以下にできるか？
+    Σ_{i} -B[i](e[i]-s[i]) + (B[i]-A[i])C[i] + (A[i]-B[i])max(0, s[i]-e[i]+C[i])
+
+    s[i] - s[i+1] <= 0 (for any i)
+    e[i] - s[0] <= T (for any i)
+    e[i] - s[i] >= 1 (for any i)
+
+    naka[i] = 1 のとき
+        e[U[i]] - s[V[i]] >= 1
+    naka[i] = -1 のとき
+        s[V[i]] >= e[U[i]]
+*/
+void AOJ_3171() {
+    long long N, M, X, INF = 1LL<<40, LIM = 1LL << 30;
+    cin >> N >> M >> X;
+    vector<long long> A(N), B(N), C(N), U(M), V(M), naka(M);
+    for (int i = 0; i < N; i++) cin >> A[i] >> B[i] >> C[i];
+    for (int i = 0; i < M; i++) cin >> U[i] >> V[i] >> naka[i], U[i]--, V[i]--;
+
+    long long low = -1, high = LIM;
+    while (high - low > 1) {
+        long long T = (low + high) / 2;
+        MinCostTension<long long> opt(N * 2);
+        for (int i = 0; i < N; i++) {
+            opt.add_cost((B[i] - A[i]) * C[i]);
+            opt.add_single_coef(i, B[i]);
+            opt.add_single_coef(i+N, -B[i]);
+            opt.add_tension_cost(i+N, i, A[i]-B[i], -C[i]);
+            if (i+1 < N) opt.add_tension_constraint(i+1, i, INF, 0);
+            opt.add_tension_constraint(0, i+N, INF, T);
+            opt.add_tension_constraint(i+N, i, INF, -1);
+        }
+        for (int i = 0; i < M; i++) {
+            if (naka[i] == 1) opt.add_tension_constraint(U[i]+N, V[i], INF, -1);
+            else opt.add_tension_constraint(V[i], U[i]+N, INF, 0);
+        }
+        auto [flag, cost] = opt.solve();
+        if (flag && cost <= -X) high = T;
+        else low = T;
+    }
+    cout << (high < LIM ? high : -1) << endl;
+}
+
 
 int main() {
     //AOJ_2230();
     //ABC_347_G();
     //ABC_397_G();
+    AOJ_3171();
 }
