2826f295ee02bbc5606e5f5acee6c6c3674faf33
drken1215 <dr.ken.1215@gmail.com>
2026-07-20 08:30:16 +0900

    add example for general matching

--- Graph/max_general_matching_by_matrix_completion.cpp
+++ Graph/max_general_matching_by_matrix_completion.cpp
@@ -12,6 +12,9 @@
 //   JOI春合宿2016 マッチングコンテスト
 //     https://atcoder.jp/contests/joisc2016matching
 //
+//   ACPC 2018 G - Combine Two Elements (AOJ 3032)
+//     https://onlinejudge.u-aizu.ac.jp/problems/3032
+//
 
 
 #pragma GCC optimize("Ofast")
@@ -19,6 +22,88 @@
 
 #include <bits/stdc++.h>
 using namespace std;
+
+
+using ll = long long;
+using i128 = __int128_t;
+using u128 = __uint128_t;
+using pint = pair<int, int>;
+using pll = pair<long long, long long>;
+using tll = array<long long, 3>;
+using fll = array<long long, 4>;
+using vint = vector<int>;
+using vll = vector<long long>;
+using dint = deque<int>;
+using dll = deque<long long>;
+using vvint = vector<vector<int>>;
+using vvll = vector<vector<long long>>;
+using vpll = vector<pair<long long, long long>>;
+template<class T> using min_priority_queue = priority_queue<T, vector<T>, greater<T>>;
+
+template<class S, class T> inline bool chmax(S &a, T b) { return (a < b ? a = b, 1 : 0); }
+template<class S, class T> inline bool chmin(S &a, T b) { return (a > b ? a = b, 1 : 0); }
+template<class S, class T> inline auto maxll(S a, T b) { return max(ll(a), ll(b)); }
+template<class S, class T> inline auto minll(S a, T b) { return min(ll(a), ll(b)); }
+template<class T> auto max(const T &a) { return *max_element(a.begin(), a.end()); }
+template<class T> auto min(const T &a) { return *min_element(a.begin(), a.end()); }
+template<class T> auto argmax(const T &a) { return max_element(a.begin(), a.end()) - a.begin(); }
+template<class T> auto argmin(const T &a) { return min_element(a.begin(), a.end()) - a.begin(); }
+template<class T> auto accum(const vector<T> &a) { return accumulate(a.begin(), a.end(), T()); }
+template<class T> auto accum(const deque<T> &a) { return accumulate(a.begin(), a.end(), T()); }
+
+#define REP(i, a) for (long long i = 0; i < (long long)(a); i++)
+#define REP2(i, a, b) for (long long i = a; i < (long long)(b); i++)
+#define RREP(i, a) for (long long i = (a)-1; i >= (long long)(0); --i)
+#define RREP2(i, a, b) for (long long i = (b)-1; i >= (long long)(a); --i)
+#define EB emplace_back
+#define PF push_front
+#define PB push_back
+#define MP make_pair
+#define FI first
+#define SE second
+#define ALL(x) x.begin(), x.end()
+#define COUT(x) cout << #x << " = " << (x) << " (L" << __LINE__ << ")" << endl
+
+// input
+template<class T> istream& operator >> (istream &is, vector<T> &P)
+{ for (int i = 0; i < (int)P.size(); ++i) cin >> P[i]; return is; }
+template<class T> istream& operator >> (istream &is, deque<T> &P)
+{ for (int i = 0; i < (int)P.size(); ++i) cin >> P[i]; return is; }
+template<class T> istream& operator >> (istream &is, vector<vector<T>> &P)
+{ for (int i = 0; i < (int)P.size(); ++i) cin >> P[i]; return is; }
+
+// output
+template<class S, class T> ostream& operator << (ostream &s, const pair<S, T> &P)
+{ return s << '<' << P.first << ", " << P.second << '>'; }
+template<class T> ostream& operator << (ostream &s, const array<T, 2> &P)
+{ return s << '<' << P[0] << "," << P[1] << '>'; }
+template<class T> ostream& operator << (ostream &s, const array<T, 3> &P)
+{ return s << '<' << P[0] << "," << P[1] << "," << P[2] << '>'; }
+template<class T> ostream& operator << (ostream &s, const array<T, 4> &P)
+{ return s << '<' << P[0] << "," << P[1] << "," << P[2] << "," << P[3] << '>'; }
+template<class T> ostream& operator << (ostream &s, const vector<T> &P)
+{ for (int i = 0; i < P.size(); ++i) { if (i > 0) { s << " "; } s << P[i]; } return s; }
+template<class T> ostream& operator << (ostream &s, const deque<T> &P)
+{ for (int i = 0; i < P.size(); ++i) { if (i > 0) { s << " "; } s << P[i]; } return s; }
+template<class T> ostream& operator << (ostream &s, const vector<vector<T>> &P)
+{ for (int i = 0; i < P.size(); ++i) { s << endl << P[i]; } return s << endl; }
+template<class T> ostream& operator << (ostream &s, const set<T> &P)
+{ for (auto it : P) { s << "<" << it << "> "; } return s; }
+template<class T> ostream& operator << (ostream &s, const multiset<T> &P)
+{ for (auto it : P) { s << "<" << it << "> "; } return s; }
+template<class T> ostream& operator << (ostream &s, const unordered_set<T> &P)
+{ for (auto it : P) { s << "<" << it << "> "; } return s; }
+template<class S, class T> ostream& operator << (ostream &s, const map<S, T> &P)
+{ for (auto it : P) { s << "<" << it.first << "->" << it.second << "> "; } return s; }
+template<class S, class T> ostream& operator << (ostream &s, const unordered_map<S, T> &P)
+{ for (auto it : P) { s << "<" << it.first << "->" << it.second << "> "; } return s; }
+void yes(bool a) { cout << (a ? "yes" : "no") << endl; }
+void YES(bool a) { cout << (a ? "YES" : "NO") << endl; }
+void Yes(bool a) { cout << (a ? "Yes" : "No") << endl; }
+const vector<int> DX = {1, 0, -1, 0, 1, -1, 1, -1};
+const vector<int> DY = {0, 1, 0, -1, 1, -1, -1, 1};
+
+
 
 /*
     グラフ G に対する Tutte 行列 T
@@ -673,8 +758,32 @@ void JOI_2026_Matching() {
     }
 }
 
+// ACPC 2018 G - Combine Two Elements (AOJ 3032)
+void AOJ_3032() {
+    int N, A, B, unit = 0;
+    cin >> N >> A >> B;
+    vector<int> a(N), b(N), used(N, false);
+    for (int i = 0; i < N; i++) {
+        cin >> a[i] >> b[i];
+        int dis = abs(a[i] - b[i]);
+        if (dis <= A || (B <= dis && dis <= A * 2)) unit++, used[i] = true;
+    }
+    vector G(N, vector(N, false));
+    for (int i = 0; i < N; i++) for (int j = i+1; j < N; j++) {
+        if (used[i] || used[j]) continue;
+        int dis = abs((a[i] + a[j]) - (b[i] + b[j]));
+        if (dis <= A || (B <= dis && dis <= A * 2)) G[i][j] = G[j][i] = true;
+    }
+    using mint = Fp<>;
+    GeneralMatching<mint> gm(G);
+    int match = gm.solve().size();
+    int res = unit + match;
+    cout << res << endl;
+}
+
 
 int main() {
     //yosupo_general_matching();
-    JOI_2026_Matching();
+    //JOI_2026_Matching();
+    AOJ_3032();
 }
